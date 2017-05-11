09b17e34faf2a321e73356ec953163985e120bab
Rodrigo Bacelli <rodrigobac@gmail.com>
2017-05-11 18:12:10 -0300

    :construction: WIP Create Header Component

---
+++ app/scripts/components/Header/Topbar.jsx
@@ -0,0 +1,11 @@
+// @flow
+
+import React from 'react';
+
+const Topbar = () => (
+  <div className="app__header__topbar">
+    Topbar
+  </div>
+);
+
+export default Topbar;
---
+++ app/scripts/containers/Config/index.jsx
@@ -0,0 +1,14 @@
+// @flow
+
+import React from 'react';
+
+const Config = (props) => {
+  console.log(props);
+  return (
+    <div>
+      Config Page
+    </div>
+  );
+};
+
+export default Config;
