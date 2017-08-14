c3a362f8d48b5fccd5e1f1cd13d6afb021c6db4c
Natália Azevedo <natalia.azevedo@printi.com.br>
2017-08-14 18:25:55 -0300

    Sprint040/ALPHA-768/MyAccountOrderListDetails

--- app/scripts/containers/MyAccount/OrderListDetails.jsx
+++ app/scripts/containers/MyAccount/OrderListDetails.jsx
@@ -62,16 +62,63 @@ export class OrderListDetails extends React.Component {
             <Warning />
           </div>
           <div>
-            <p>Atenção! Você ainda não enviou o arquivo de alguns produtos. A previsão de entrega esta sujeita a alteração caso o arquivo não seja enviado até dia 16/04/2015.</p>
+            <p><span>Atenção!</span> Você ainda não enviou o arquivo de alguns produtos. A previsão de entrega esta sujeita a alteração caso o arquivo não seja enviado até dia 16/04/2015.</p>
+          </div>
+          <div>
+            <p>x</p>
           </div>
         </div>
 
-        <div>
-          <div>
-            first box
+        <div className="box-paymentDetails-dataDelivery">
+          <div className="box-paymentDetails">
+            <div className="box-headerDetails">
+              <h4 className="title-details">Dados de pagamento</h4>
+              <Link to="#" className="link-alterDetails">Alterar </Link>
+            </div>
+            <div className="paymentDetails-dataDelivery">
+              <div className="details">
+                <p className="firstDetail">Nome</p>
+                <p className="secondDetail">Diogo Capelo</p>
+              </div>
+              <div className="details">
+                <p className="firstDetail">Endereço</p>
+                <p className="secondDetail">Av. Brigadeiro Faria Lima, 1451 - Apt 102 Torre Pequim - Cocala</p>
+              </div>
+              <div className="details">
+                <p className="firstDetail">Cidade/UF</p>
+                <p className="secondDetail">Guarulhos/SP</p>
+              </div>
+              <div className="details">
+                <p className="firstDetail">CEP</p>
+                <p className="secondDetail">07130-000</p>
+              </div>
+              <p className="paymentMethod-details">forma de pagamento: <span className="typeMethod">boleto</span></p>
+            </div>
           </div>
-          <div>
-            second box
+
+          <div className="box-dataDelivery">
+            <div className="box-headerDetails">
+              <h4 className="title-details">Entrega</h4>
+              <Link to="#" className="link-alterDetails">Alterar </Link>
+            </div>
+            <div className="paymentDetails-dataDelivery">
+              <div className="details">
+                <p className="firstDetail">Nome</p>
+                <p className="secondDetail">Diogo Capelo</p>
+              </div>
+              <div className="details">
+                <p className="firstDetail">Endereço</p>
+                <p className="secondDetail">Av. Brigadeiro Faria Lima, 1451 - Apto 102 Torre Pequim - Cocala</p>
+              </div>
+              <div className="details">
+                <p className="firstDetail">Cidade/UF</p>
+                <p className="secondDetail">Guarulhos/SP</p>
+              </div>
+              <div className="details">
+                <p className="firstDetail">CEP</p>
+                <p className="secondDetail">07130-000</p>
+              </div>
+            </div>
           </div>
         </div>
 
