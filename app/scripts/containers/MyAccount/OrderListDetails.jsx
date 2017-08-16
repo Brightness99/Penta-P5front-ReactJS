6f62beb9297c1ca60a1adbbb8b33f8f55db149dc
Natália Azevedo <natalia.azevedo@printi.com.br>
2017-08-16 15:15:29 -0300

    Sprint040|ALPHA-768/MyOrderListDetails desktop

--- app/scripts/containers/MyAccount/OrderListDetails.jsx
+++ app/scripts/containers/MyAccount/OrderListDetails.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Breadcrumbs from 'components/Breadcrumbs';
 import { Link } from 'react-router-dom';
-import { CodeBar, CheckIcon, Receipt, ExclamationMark, CloseIcon, DocumentDownload, Clipboard, Cards, Warning, Change, Archive } from 'components/Icons';
+import { CodeBar, CheckIcon, Receipt, ExclamationMark, CloseIcon, Warning, Change, Archive } from 'components/Icons';
 
 type Props = {
   screenSize: string,
@@ -18,7 +18,141 @@ export class OrderListDetails extends React.Component {
   renderMobile() {
     return (
       <section className="container-myaccountdetails">
-        <p>Mobile</p>
+        <h2>Minha conta</h2>
+        <div className="box-headerDetails">
+          <div>
+            <h3 className="subtitle-details">Pedido nº483.093</h3>
+            <p className="total-value">Valor total: <span>R$ 350,00</span></p>
+          </div>
+        </div>
+        <div className="box-warningDetails">
+          <div>
+            <i><Warning /></i>
+          </div>
+          <div>
+            <p><span>Atenção!</span> Você ainda não enviou o arquivo de alguns produtos. A previsão de entrega esta sujeita a alteração caso o arquivo não seja enviado até dia 16/04/2015.</p>
+          </div>
+          <div>
+            <i><CloseIcon /></i>
+          </div>
+        </div>
+        <div className="btns-details">
+          <Link to="#" className="btn-default btn-quarter fnt-bold btn-lg"><i><CodeBar /></i>Imprimir boleto</Link>
+          <Link to="#" className="btn-default btn-quarter fnt-bold btn-lg"><i><Receipt /></i>Enviar comprovante</Link>
+        </div>
+        <div className="box-paymentDetails-dataDelivery">
+          <div className="box-paymentDetails">
+            <div className="box-headerDetails">
+              <h4 className="title-details">Dados de pagamento</h4>
+            </div>
+            <div className="paymentDetails-dataDelivery">
+              <div className="details">
+                <p className="firstDetail">Nome</p>
+                <p className="secondDetail">Diogo Capelo</p>
+              </div>
+              <div className="details">
+                <p className="firstDetail">Endereço</p>
+                <p className="secondDetail">Av. Brigadeiro Faria Lima, 1451 - Apt 102 Torre Pequim - Cocala</p>
+              </div>
+              <div className="details">
+                <p className="firstDetail">Cidade/UF</p>
+                <p className="secondDetail">Guarulhos/SP</p>
+              </div>
+              <div className="details">
+                <p className="firstDetail">CEP</p>
+                <p className="secondDetail">07130-000</p>
+              </div>
+              <p className="paymentMethod-details">forma de pagamento: <span className="typeMethod">boleto</span></p>
+              <Link to="#" className="link-alterDetails--mobile"><Change />Alterar forma de pagamento</Link>
+            </div>
+          </div>
+          <div className="box-dataDelivery">
+            <div className="box-headerDetails">
+              <h4 className="title-details">Entrega</h4>
+            </div>
+            <div className="paymentDetails-dataDelivery">
+              <div className="details">
+                <p className="firstDetail">Nome</p>
+                <p className="secondDetail">Diogo Capelo</p>
+              </div>
+              <div className="details">
+                <p className="firstDetail">Endereço</p>
+                <p className="secondDetail">Av. Brigadeiro Faria Lima, 1451 - Apto 102 Torre Pequim - Cocala</p>
+              </div>
+              <div className="details">
+                <p className="firstDetail">Cidade/UF</p>
+                <p className="secondDetail">Guarulhos/SP</p>
+              </div>
+              <div className="details">
+                <p className="firstDetail">CEP</p>
+                <p className="secondDetail">07130-000</p>
+              </div>
+              <Link to="#" className="link-alterDetails--mobile"><Change />Alterar dados de entrega</Link>
+            </div>
+          </div>
+        </div>
+        <div className="box-detailsAboutProduct allright">
+          <div className="header-detailsAboutProduct">
+            <h4 className="title-detailsAboutProduct">Cartão de visita</h4>
+            <p className="title-numberOrder">[item nº 593328]</p>
+            <Link to="#">
+              + Mais detalhes
+            </Link>
+          </div>
+          <div className="box-aboutProduct">
+            <ul className="status-aboutProduct">
+              <li className="allright">
+                <span>1</span>
+              </li>
+              <li>
+                <span>2</span>
+              </li>
+              <li>
+                <span>3</span>
+              </li>
+              <li>
+                <span>4</span>
+              </li>
+            </ul>
+            <div className="box-detailsTransport">
+              <div className="details-product">
+                <div className="detail imageProduct">
+                  <div className="title-imageProduct--mobile">
+                    <p className="title--mobile">Arquivo</p>
+                  </div>
+                  <div>
+                    <div className="box-imageProduct--mobile">
+                      <div>
+                        <img src={require('assets/media/images/captura-de-tela.png')} alt="imagem" />
+                      </div>
+                      <div>
+                        <h5 className="title-status"><i><CheckIcon /></i>Enviado</h5>
+                        <p>A imagem ao lado é meramente ilustrativa.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="detail">
+                  <div className="title-imageProduct--mobile">
+                    <p className="title--mobile">Datas</p>
+                  </div>
+                  <div>
+                    <p className="title-data">20/04/2015</p>
+                    <p>Previsão de entrega</p>
+                  </div>
+                </div>
+                <div className="detail">
+                  <div className="title-imageProduct--mobile">
+                    <p className="title--mobile">Ações</p>
+                  </div>
+                  <div>
+                    <Link className="btn-default btn-quarter fnt-sbold btn-lg" to="#"><i><Archive /></i>Acompanhar o pedido</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
@@ -45,7 +179,6 @@ export class OrderListDetails extends React.Component {
       <section className="container-myaccountdetails">
         <Breadcrumbs links={breadcrumb} />
         <h2>Minha conta</h2>
-
         <div className="box-headerDetails">
           <div>
             <h3 className="subtitle-details">Pedido nº483.093</h3>
@@ -56,7 +189,6 @@ export class OrderListDetails extends React.Component {
             <Link to="#" className="btn-default btn-quarter fnt-bold btn-lg"><i><Receipt /></i>Enviar comprovante</Link>
           </div>
         </div>
-
         <div className="box-warningDetails">
           <div>
             <i><Warning /></i>
@@ -68,7 +200,6 @@ export class OrderListDetails extends React.Component {
             <i><CloseIcon /></i>
           </div>
         </div>
-
         <div className="box-paymentDetails-dataDelivery">
           <div className="box-paymentDetails">
             <div className="box-headerDetails">
@@ -95,7 +226,6 @@ export class OrderListDetails extends React.Component {
               <p className="paymentMethod-details">forma de pagamento: <span className="typeMethod">boleto</span></p>
             </div>
           </div>
-
           <div className="box-dataDelivery">
             <div className="box-headerDetails">
               <h4 className="title-details">Entrega</h4>
@@ -121,7 +251,6 @@ export class OrderListDetails extends React.Component {
             </div>
           </div>
         </div>
-
         <div className="box-detailsAboutProduct allright">
           <div className="header-detailsAboutProduct">
             <h4 className="title-detailsAboutProduct">Cartão de visita</h4>
@@ -142,7 +271,7 @@ export class OrderListDetails extends React.Component {
                 <li>Arquivo</li>
                 <li>Datas</li>
                 <li>Ações</li>
-              </ul>                                                         
+              </ul>
               <div className="details-product">
                 <div className="detail imageProduct">
                   <div>
@@ -164,7 +293,6 @@ export class OrderListDetails extends React.Component {
             </div>
           </div>
         </div>
-
         <div className="box-detailsAboutProduct attention">
           <div className="header-detailsAboutProduct">
             <h4 className="title-detailsAboutProduct">Cartão de visita</h4>
@@ -185,7 +313,7 @@ export class OrderListDetails extends React.Component {
                 <li>Arquivo</li>
                 <li>Datas</li>
                 <li>Ações</li>
-              </ul>                                                      
+              </ul>
               <div className="details-product">
                 <div className="detail imageProduct">
                   <div>
@@ -213,7 +341,6 @@ export class OrderListDetails extends React.Component {
             </div>
           </div>
         </div>
-
       </section>
     );
   }
