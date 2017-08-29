076848d46665c44badba5d1de41067ee3926cfdb
Rodrigo Bacelli <rodrigobac@gmail.com>
2017-08-29 17:17:59 -0300

    Sprint041|ALPHA-785|Add exclusive service and models drop down

--- app/scripts/components/Header/_ExclusiveService.jsx
+++ app/scripts/components/Header/_ExclusiveService.jsx
@@ -5,27 +5,34 @@ import { NavLink } from "react-router-dom";
 
 const ExclusiveService = () => {
   return (
-    <div className="mol-exclusive-service">
+    <div className="mol-header-button-hidden mol-exclusive-service">
       <div className="atm-excusive-service-title">Este atendimento é exclusivo para clientes com:</div>
       <ul className="mol-exclusive-service-list">
-        <li className="money">
-          <p className="money">+5000</p>
-          <div className="atm-sprite-icon atm-sprite-icon--money" />
-          <p className="ou">ou</p>
-          <p className="excdesc">Pedidos superiores a R$ 5.000,00</p>
-          <div className="line" />
+        <li className="mol-exlusive-service-item">
+          <div className="atm-exlusive-service-item-title">
+            +5000 <div className="atm-sprite-icon atm-sprite-icon--money" />
+          </div>
+          <p>Pedidos superiores a R$ 5.000,00</p>
         </li>
-        <li className="people">
-          <p className="people">+50</p>
-          <div className="atm-sprite-icon atm-sprite-icon--people" />
-          <p className="ou">ou</p>
-          <p className="excdesc">Porte superior a 50 funcionários</p>
-          <div className="line" />
+        <li className="mol-exclusive-service-separator">
+          <span>ou</span>
+          <div className="mol-exlusive-service-item-line" />
         </li>
-        <li className="house">
-          <p className="house">+10</p>
-          <div className="atm-sprite-icon atm-sprite-icon--house" />
-          <p className="excdesc">Empresa com 10 unidades ou mais</p>
+        <li className="mol-exlusive-service-item">
+          <div className="atm-exlusive-service-item-title">
+            +50 <div className="atm-sprite-icon atm-sprite-icon--people" />
+          </div>
+          <p>Porte superior a 50 funcionários</p>
+        </li>
+        <li className="mol-exclusive-service-separator">
+          <span>ou</span>
+          <div className="mol-exlusive-service-item-line" />
+        </li>
+        <li className="mol-exlusive-service-item">
+          <div className="atm-exlusive-service-item-title">
+            +10 <div className="atm-sprite-icon atm-sprite-icon--house" />
+          </div>
+          <p>Empresa com 10 unidades ou mais</p>
         </li>
       </ul>
       <NavLink to="/venda-corporativa" className="atm-regular-button">
