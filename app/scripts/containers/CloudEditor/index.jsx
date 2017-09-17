35528bf4ceb0f8f2ccaf21744820db97350404a3
Natália Azevedo <natalia.azevedo@printi.com.br>
2017-09-17 14:39:43 -0300

    Sprint042 | Cloud editor

--- app/scripts/containers/CloudEditor/index.jsx
+++ app/scripts/containers/CloudEditor/index.jsx
@@ -1,7 +1,17 @@
 // @flow
 import React from 'react';
+import { connect } from 'react-redux';
+
+type Props = {
+  app: AppStore,
+  router: RouterStore,
+  dispatch: () => {},
+  children: any,
+};
 
 class CloudEditor extends React.Component {
+  static props: Props;
+
   render() {
     return (
       <p> editor</p>
@@ -9,4 +19,16 @@ class CloudEditor extends React.Component {
   }
 }
 
-export default CloudEditor;
+/* istanbul ignore next */
+function mapStoreToProps(state) {
+  return ({
+    app: state.app,
+  });
+}
+
+/* istanbul ignore next */
+function mapDispatchToProps(dispatch) {
+  return { dispatch };
+}
+
+export default connect(mapStoreToProps, mapDispatchToProps)(CloudEditor);
