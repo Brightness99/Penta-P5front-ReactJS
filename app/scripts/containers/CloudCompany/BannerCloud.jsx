18730d5331c9070c143cf52871a2da6271d4d378
Natália Azevedo <natalia.azevedo@printi.com.br>
2017-09-17 14:03:23 -0300

    Sprint042 | Cloud page in mobile

--- app/scripts/containers/CloudCompany/BannerCloud.jsx
+++ app/scripts/containers/CloudCompany/BannerCloud.jsx
@@ -1,68 +1,36 @@
 // @flow
 import React from 'react';
 import { Link } from 'react-router-dom';
-import cx from 'classnames';
 import { isMobile } from 'utils/helpers';
+import cx from 'classnames';
 
 type Props = {
   screenSize: string,
 };
 
 export class BannerCloud extends React.Component {
-  static defaultProps = {
-    screenSize: 'xs',
-  };
-
   props: Props;
 
-  /*renderMobile() {
+  render() {
+    const { screenSize } = this.props;
     return (
-      <div className="org-banner-cloud bg-mobile">
-        <div className="mol-banner-cloud">
-          <div className="atm-banner-cloud">
-            <div className="container">
+      <div className={cx('org-banner-cloud', isMobile(screenSize) && ('bg-mobile'))}>
+        <div className="container">
+          <div className="mol-banner-cloud">
+            <div className="atm-banner-cloud">
               <h2 className="qrk-title-banner-cloud">Conheça a cloud</h2>
               <h3 className="qrk-subtitle-banner-cloud">A ferramenta que agiliza seus materiais gráficos</h3>
               <p className="qrk-text-banner-cloud">Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo.</p>
               <Link to="#" className="btn-default btn-secondary btn-sm">Quero saber mais</Link>
             </div>
-          </div>
-        </div>
-      </div>
-    );
-  }*/
-
-  render() {
-    const screenSize = this.props;
-    return (
-      <div className="org-banner-cloud">
-        <div className={isMobile(screenSize) && 'bg-mobile'}
-        >
-          <div className="container">
-            <div className="mol-banner-cloud">
-              <div className="atm-banner-cloud">
-                <h2 className="qrk-title-banner-cloud">Conheça a cloud</h2>
-                <h3 className="qrk-subtitle-banner-cloud">A ferramenta que agiliza seus materiais gráficos</h3>
-                <p className="qrk-text-banner-cloud">Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo.</p>
-                <Link to="#" className="btn-default btn-secondary btn-sm">Quero saber mais</Link>
-              </div>
-              {!isMobile(screenSize) &&
-                <img src={require('assets/media/images/cloud-banner.png')} alt="Cloud" />
-              }
-            </div>
+            {!isMobile(screenSize) &&
+              <img src={require('assets/media/images/cloud-banner.png')} alt="Cloud" />
+            }
           </div>
         </div>
       </div>
     );
   }
-
-  /*render() {
-    const { screenSize } = this.props;
-
-    return ['xs', 'is', 'sm', 'ix', 'md', 'im'].includes(screenSize)
-      ? this.renderMobile()
-      : this.renderDesktop();
-  }*/
 }
 
 export default BannerCloud;
