0237c665403fdad74ce44ea098a25164f4e12d13
Natália Azevedo <natalia.azevedo@printi.com.br>
2017-09-18 09:34:46 -0300

    Sprint042 | Mobile style

--- app/scripts/containers/Glossary/AlphabetList.jsx
+++ app/scripts/containers/Glossary/AlphabetList.jsx
@@ -1,39 +1,43 @@
 // @flow
 
 import React from 'react';
+import { isMobile } from 'utils/helpers';
+import cx from 'classnames';
 
 class AlphabetList extends React.Component {
   render() {
     return (
-      <ul className="org-alphabet-list">
-        <li>#</li>
-        <li>A</li>
-        <li>B</li>
-        <li>C</li>
-        <li>D</li>
-        <li>E</li>
-        <li>F</li>
-        <li>G</li>
-        <li>H</li>
-        <li>I</li>
-        <li>J</li>
-        <li>K</li>
-        <li>L</li>
-        <li>M</li>
-        <li>N</li>
-        <li>O</li>
-        <li>P</li>
-        <li>Q</li>
-        <li>R</li>
-        <li>S</li>
-        <li>T</li>
-        <li>U</li>
-        <li>V</li>
-        <li>W</li>
-        <li>X</li>
-        <li>Y</li>
-        <li>Z</li>
-      </ul>
+      <div className="scroll-list-mobile">
+        <ul className="org-alphabet-list">
+          <li>#</li>
+          <li>A</li>
+          <li>B</li>
+          <li>C</li>
+          <li>D</li>
+          <li>E</li>
+          <li>F</li>
+          <li>G</li>
+          <li>H</li>
+          <li>I</li>
+          <li>J</li>
+          <li>K</li>
+          <li>L</li>
+          <li>M</li>
+          <li>N</li>
+          <li>O</li>
+          <li>P</li>
+          <li>Q</li>
+          <li>R</li>
+          <li>S</li>
+          <li>T</li>
+          <li>U</li>
+          <li>V</li>
+          <li>W</li>
+          <li>X</li>
+          <li>Y</li>
+          <li>Z</li>
+        </ul>
+      </div>
     );
   }
 }
