881ecac0988befff94df4f2afdc44500165e13ab
Rodrigo Bacelli <rodrigobac@gmail.com>
2017-09-17 23:07:38 -0300

    Sprint040|ALPHA-749|Product page highlights

--- app/scripts/components/LandingPage/index.js
+++ app/scripts/components/LandingPage/index.js
@@ -5,3 +5,4 @@ export BlogBlock from './Blog';
 export CategoriesCarouselBlock from './CategoriesCarousel';
 export CustomersRelyBlock from './CustomersRely';
 export HighlightsBlock from './Highlights';
+export ProductHighlightsBlock from './ProductHighlights';
