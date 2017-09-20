0fa1500849c00ea0809f1d51e41435cbbe749357
naat_azevedo <natalia.azevedo@printi.com.br>
2017-09-20 17:24:55 -0300

    Sprint042 | The first part about this page

--- app/scripts/containers/AboutPrinti/index.jsx
+++ app/scripts/containers/AboutPrinti/index.jsx
@@ -6,6 +6,8 @@ import { Link } from 'react-router-dom';
 import { isMobile } from 'utils/helpers';
 import Breadcrumbs from 'components/Breadcrumbs';
 
+import AboutPrintiText from './AboutPrintiText';
+
 type Props = {
   app: AppStore,
   router: RouterStore,
@@ -32,24 +34,10 @@ export class AboutPrinti extends React.Component {
       <section>
         {!isMobile(screenSize) && <Breadcrumbs links={breadcrumb} />}
         <div className="container">
-          <h2>Sobre a printi</h2>
-
-          <h3>Revolucionando o mercado gráfico</h3>
-          <p>A Printi está no mercado desde 2012</p>
-          
-          <p>A Printi é um modelo inovador de gráfica online no mercado brasileiro, com impressões de alta qualidade a preços acessíveis.
-            Utilizamos as mais modernas ferramentas do setor de Web2Print para simplificar e agilizar o complexo processo de encomenda de impressos personalizados.
-          </p>
-          <p>Fundada a partir de conceitos adquiridos em 25 anos de experiência na indústria gráfica brasileira, a Printi oferece mais de 6.000 combinações diferentes de produtos impressos.
-            Todas essas combinações são acessíveis através de nossa revolucionária matriz de customização, que dá ao usuário o total controle de seu produto, e calcula, em trempo real, o custo final do orçamento.
-            É essa funcionalidade que traz completa transparência ao processo de compra e que separa a Printi de seus concorrentes no mercado gráfico.
-          </p>
-          <div>
-            <p>"A Printi oferece a checagem online gratuita e imediata da arte final, uma característica oferecida somente pelos mais avançados serviços Web2Print do mundo"</p>
+          <div className="tpl-about-printi">
+            <h2 className="title-about-printi">Sobre a printi</h2>
+            <AboutPrintiText />
           </div>
-
-          <p>Além da revolucionária matriz de customização, a Printi oferece a checagem online gratuita e imediata da arte final, uma característica oferecida somente pelos mais avançados serviços Web2Print do mundo. A checagem online elimina a maior parte do ineficiente processo de pré-impressão, fornecendo ao cliente final uma experiência rápida e eficiente, sem burocracia e sem transtornos. Essa eficiência permite à Printi repassar a seus clientes as reduções de custo obtidas, assim oferecendo preços excelentes e produtos de altíssima qualidade.</p>
-          <p>A Printi convida todos a testarem nosso processo de customização e compra. Em caso de dúvidas, entre em contato conosco através do email suporte@printi.com.br ou do telefone (11) 2318.1919.</p>
         </div>
       </section>
     );
