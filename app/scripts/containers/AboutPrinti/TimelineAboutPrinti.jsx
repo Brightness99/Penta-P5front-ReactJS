2ba4258a570d656c004613f59a346abbeb81b6f4
naat_azevedo <natalia.azevedo@printi.com.br>
2017-09-21 09:54:49 -0300

    Sprint042 | Timeline in About Printi

--- app/scripts/containers/AboutPrinti/TimelineAboutPrinti.jsx
+++ app/scripts/containers/AboutPrinti/TimelineAboutPrinti.jsx
@@ -1,5 +1,5 @@
 // @flow
-
+// https://codyhouse.co/demo/vertical-timeline/index.html
 import React from 'react';
 
 export class TimelineAboutPrinti extends React.Component {
@@ -10,14 +10,24 @@ export class TimelineAboutPrinti extends React.Component {
           <h4>A Printi não para de crescer</h4>
           <p>Confira um pouco da história da nossa evolução</p>
 
-          <div className="container-timeline">
-            <div className="post-one">
-              <div className="icon-post">
-                icone
+          <div className="mol-timeline">
+            <div className="atm-card-timeline">
+              <div className="qrk-number-card">
+                1
               </div>
-              <div>
-                container text
+
+              <div className="qrk-text-card">
+                <div>
+                  <p>10 de junho 2012</p>
+                  <p>Como tudo começou</p>
+                </div>
+                <img src={require('assets/media/images/como-tudo-comecou.png')} alt="Como tudo começou" />
+                <div>
+                  <p>CRAS JUSTO DAPIBUS</p>
+                  <p>Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                </div>
               </div>
+
             </div>
           </div>
 
