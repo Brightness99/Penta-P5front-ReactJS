af05c9c022eb45e8f6001c585f262525d2d3ca1c
naat_azevedo <natalia.azevedo@printi.com.br>
2017-09-21 12:19:39 -0300

    Sprint042 | Layout about printi

--- app/scripts/containers/AboutPrinti/TimelineAboutPrinti.jsx
+++ app/scripts/containers/AboutPrinti/TimelineAboutPrinti.jsx
@@ -7,27 +7,73 @@ export class TimelineAboutPrinti extends React.Component {
     return (
       <div className="org-timeline-printi">
         <div className="container">
-          <h4>A Printi não para de crescer</h4>
-          <p>Confira um pouco da história da nossa evolução</p>
+          <h4 className="title-timeline">A Printi não para de crescer</h4>
+          <p className="subtitle-timeline">Confira um pouco da história da nossa evolução</p>
 
           <div className="mol-timeline">
+            <span className="atm-line-timeline" />
             <div className="atm-card-timeline">
-              <div className="qrk-number-card">
-                1
-              </div>
+              <span className="qrk-number-card">1</span>
 
               <div className="qrk-text-card">
-                <div>
-                  <p>10 de junho 2012</p>
-                  <p>Como tudo começou</p>
+                <div className="bsn-title-card">
+                  <p className="title-data">10 de junho 2012</p>
+                  <p className="title-card">Como tudo começou</p>
                 </div>
                 <img src={require('assets/media/images/como-tudo-comecou.png')} alt="Como tudo começou" />
-                <div>
-                  <p>CRAS JUSTO DAPIBUS</p>
-                  <p>Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                <div className="bsn-text-card">
+                  <p className="title-text-card">CRAS JUSTO DAPIBUS</p>
+                  <p className="text-card">Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
                 </div>
               </div>
+            </div>
 
+            <div className="atm-card-timeline">
+              <span className="qrk-number-card">2</span>
+
+              <div className="qrk-text-card">
+                <div className="bsn-title-card">
+                  <p className="title-data">10 de junho 2012</p>
+                  <p className="title-card">Como tudo começou</p>
+                </div>
+                <img src={require('assets/media/images/como-tudo-comecou.png')} alt="Como tudo começou" />
+                <div className="bsn-text-card">
+                  <p className="title-text-card">CRAS JUSTO DAPIBUS</p>
+                  <p className="text-card">Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="atm-card-timeline">
+              <span className="qrk-number-card">3</span>
+
+              <div className="qrk-text-card">
+                <div className="bsn-title-card">
+                  <p className="title-data">10 de junho 2012</p>
+                  <p className="title-card">Como tudo começou</p>
+                </div>
+                <img src={require('assets/media/images/como-tudo-comecou.png')} alt="Como tudo começou" />
+                <div className="bsn-text-card">
+                  <p className="title-text-card">CRAS JUSTO DAPIBUS</p>
+                  <p className="text-card">Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="atm-card-timeline">
+              <span className="qrk-number-card">4</span>
+
+              <div className="qrk-text-card">
+                <div className="bsn-title-card">
+                  <p className="title-data">10 de junho 2012</p>
+                  <p className="title-card">Como tudo começou</p>
+                </div>
+                <img src={require('assets/media/images/como-tudo-comecou.png')} alt="Como tudo começou" />
+                <div className="bsn-text-card">
+                  <p className="title-text-card">CRAS JUSTO DAPIBUS</p>
+                  <p className="text-card">Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                </div>
+              </div>
             </div>
           </div>
 
