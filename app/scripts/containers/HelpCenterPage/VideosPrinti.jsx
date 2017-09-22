24891630f503a268b2f72a68884a21f9cda75c8e
naat_azevedo <natalia.azevedo@printi.com.br>
2017-09-21 23:15:35 -0300

    Sprint042 | Layout helpcenter and contact desktop

---
+++ app/scripts/containers/HelpCenterPage/VideosPrinti.jsx
@@ -0,0 +1,18 @@
+// @flow 
+
+import React from 'react';
+
+export class VideosPrinti extends React.Component {
+  render() {
+    return (
+      <div className="org-videos-printi">
+        <h4>Nossos Vídeos</h4>
+        <div className="mol-videos-printi">
+          <div className="atm-video">video grande</div>
+          <div className="atm-video">video pequeno</div>
+        </div>
+      </div>
+    );
+  }
+}
+export default VideosPrinti;
