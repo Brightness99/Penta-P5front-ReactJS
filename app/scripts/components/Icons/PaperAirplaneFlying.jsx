9fe048e1163386735a63d8f88d538b771c5b742b
naat_azevedo <natalia.azevedo@printi.com.br>
2017-09-21 22:52:24 -0300

    Sprint042 | Help center layout

---
+++ app/scripts/components/Icons/PaperAirplaneFlying.jsx
@@ -0,0 +1,11 @@
+// @flow
+
+import React from 'react';
+
+const PaperAirplaneFlying = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 466.897 466.897">
+    <polygon points="374.105,73.807 203.974,318.03 203.974,432.042 259.801,358.275 410.505,466.897     441.012,0 25.885,189.672 143.823,274.681" />
+  </svg>
+);
+
+export default PaperAirplaneFlying;
---
+++ app/scripts/containers/HelpCenterPage/RelationshipCentral.jsx
@@ -0,0 +1,51 @@
+// @flow
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ChatIcon, PhoneIcon, PaperAirplaneFlying } from 'components/Icons';
+
+export class RelationshipCentral extends React.Component {
+  render() {
+    return (
+      <div className="org-relationship-central">
+        <h3>Central de Relacionamento e-commerce</h3>
+        <div className="mol-relationship-central">
+          <div className="atm-link-central">
+            <div className="qrk-link-central">
+              <div>
+                <ChatIcon />
+                <p>Chat</p>
+              </div>
+              <Link to="#" className="btn-default btn-third btn-xs">Iniciar Atendimento</Link>
+            </div>
+            <div className="qrk-link-central">
+              <div>
+                <PaperAirplaneFlying />
+                <p>E-mail</p>
+              </div>
+              <Link to="#" className="btn-default btn-third btn-xs">Enviar um e-mail</Link>
+            </div>
+            <div className="qrk-link-central">
+              <div>
+                <PhoneIcon />
+                <p>Telefone</p>
+              </div>
+              <div className="bsn-links-tel">
+                <Link to="#" className="btn-default btn-third btn-xs">A printi liga pra você</Link>
+                <Link to="#" className="btn-default btn-third btn-xs">Você liga para a printi</Link>
+              </div>
+            </div>
+          </div>
+          <h4>Venda corporativa</h4>
+          <div className="">
+            Atendimento Corporativo
+            <Link to="#" className="btn-default btn-third btn-xs">Quero saber mais</Link>
+            <Link to="#" className="btn-default btn-third btn-xs">Solicitar atendimento</Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RelationshipCentral;
