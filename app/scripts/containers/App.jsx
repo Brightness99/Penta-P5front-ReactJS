70e2a94bdf9193b7bff75b41b5ffa602949b4dd2
Boikov <boikovdmitriy@mail.ru>
2017-09-24 19:58:48 +0300

    Sprint 43 | ALPHA-948 | added logout, added switching between loggenIn and loggedOut mode in the MyAccount

--- app/scripts/containers/App.jsx
+++ app/scripts/containers/App.jsx
@@ -34,6 +34,7 @@ type Props = {
   dispatch: () => {},
   router: RouterStore,
   locale: {},
+  isAuthorized: boolean
 };
 
 export class App extends React.Component {
@@ -59,7 +60,7 @@ export class App extends React.Component {
   };
 
   render() {
-    const { app, dispatch, router, cart } = this.props;
+    const { app, dispatch, router, cart, isAuthorized } = this.props;
 
     let html = (<div className="loader">Loading</div>);
 
@@ -72,7 +73,10 @@ export class App extends React.Component {
               path="/"
               render={() => (
                 <div key="app" className="app">
-                  <Header screenSize={app.screenSize} dispatch={dispatch} totalCartItems={cart.count} />
+                  <Header
+                    screenSize={app.screenSize} dispatch={dispatch} totalCartItems={cart.count}
+                    isAuthorized={isAuthorized}
+                  />
                   <main className="app__main">
                     <Switch>
                       <Route exact path="/" component={Home} />
@@ -115,6 +119,7 @@ function mapStateToProps(state) {
     cart: state.cart,
     router: state.router,
     locale: state.locale,
+    isAuthorized: state.user.isAuthorized,
   };
 }
 
