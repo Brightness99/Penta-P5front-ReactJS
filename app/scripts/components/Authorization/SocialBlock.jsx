3de1351e59dc626d92f19c2f7fd5b48c844c6a51
Boikov <boikovdmitriy@mail.ru>
2017-09-25 18:12:02 +0300

    Sprint 43 | ALPHA-948 | added social buttons, fixed logout url in the epic

--- app/scripts/components/Authorization/SocialBlock.jsx
+++ app/scripts/components/Authorization/SocialBlock.jsx
@@ -1,24 +1,167 @@
 // @flow
 import React from 'react';
 import SVG from 'react-inlinesvg';
-import { Button } from 'quarks/Inputs';
+import GoogleLogin from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
+
+type GoogleSuccessResult ={
+  googleId: string,
+  tokenId: string,
+  accessToken: string,
+  tokenObj: Object,
+  profileObj: Object
+}
+
+type GoogleFailureResult ={
+  error: string,
+  details: string
+}
+
+type FBSuccessResult ={
+  status: string,
+  authResponse: {
+    accessToken: string,
+    expiresIn: string,
+    signedRequest: string,
+    userID: string
+  }
+}
+
+type FBFailureResult ={
+}
+
+type SocialLoginResult ={
+  socialType: string,
+  socialData: {
+    socialId: string,
+    socialToken: string,
+  },
+  isSuccess: boolean,
+  error: {}
+}
 
 type Props = {
-
+    facebook: {
+      enabled: boolean,
+      credentials: {
+        app_id: string,
+        secret_key: string,
+      }
+    },
+    google: {
+      enabled: boolean,
+      credentials: {
+        client_id: string,
+        secret_key: string,
+      }
+    },
+    loginFBSuccess: (result: SocialLoginResult)=>void,
+    loginFBFailure: (result: SocialLoginResult)=>void,
+    loginGoogleSuccess: (result: SocialLoginResult)=>void,
+    loginGoogleFailure: (result: SocialLoginResult)=>void,
 };
 
 export default class SocialBlock extends React.PureComponent {
   props: Props;
 
+  loginFBSuccess=(data: FBSuccessResult) => {
+    const { loginFBSuccess } = this.props;
+    if (!data.status) return;
+
+    const result = {
+      socialType: 'facebook',
+      socialData: {
+        socialId: data.authResponse.userID,
+        socialToken: data.authResponse.accessToken,
+      },
+      isSuccess: true,
+    };
+    loginFBSuccess(result);
+  };
+
+  loginFBFailure= (data: FBFailureResult) => {
+    const { loginFBFailure } = this.props;
+    if (typeof loginFBFailure !== 'function') { return; }
+
+    const result = {
+      socialType: 'facebook',
+      isSuccess: false,
+      error: data,
+    };
+
+    loginFBFailure(result);
+  };
+
+  loginGoogleSuccess= (data: GoogleSuccessResult) => {
+    const { loginGoogleSuccess } = this.props;
+
+    const result = {
+      socialType: 'google',
+      socialData: {
+        socialId: data.googleId,
+        socialToken: data.tokenId,
+      },
+      isSuccess: true,
+    };
+
+    loginGoogleSuccess(result);
+  };
+
+  loginGoogleFailure= (data: GoogleFailureResult) => {
+    const { loginGoogleFailure } = this.props;
+    if (typeof loginGoogleFailure !== 'function') { return; }
+    const result = {
+      socialType: 'google',
+      error: data,
+      isSuccess: false,
+    };
+
+    loginGoogleFailure(result);
+  };
+
+  renderFacebookLogin = () => {
+    const { facebook } = this.props;
+    if (facebook.enabled) {
+      return (
+        <FacebookLogin
+          appId={facebook.credentials.app_id}
+          autoLoad={true}
+          fields="name,email,picture"
+          textButton={''}
+          callback={this.loginFBSuccess}
+          onFailure={this.loginFBFailure}
+          cssClass={'facebook-login-button app__qrk__button'}
+          icon={<SVG src={require('assets/media/svg/social_facebook.svg')} />}
+        />
+      );
+    }
+    return '';
+  };
+
+  renderGoogleLogin = () => {
+    const { google } = this.props;
+
+    if (google.enabled) {
+      return (
+        <GoogleLogin
+          clientId={google.credentials.client_id}
+          buttonText={''}
+          onSuccess={this.loginGoogleSuccess}
+          onFailure={this.loginGoogleFailure}
+          className={'google-login-button app__qrk__button'}
+        >
+          <SVG src={require('assets/media/svg/social_googleplus.svg')} />
+        </GoogleLogin>
+      );
+    }
+    return '';
+  };
+
   render() {
     return (
       <div className="authentication__social">
-        <Button className="authentication__social__button facebook">
-          <SVG src={require('assets/media/svg/social_facebook.svg')} />
-        </Button>
-        <Button className="authentication__social__button google">
-          <SVG src={require('assets/media/svg/social_googleplus.svg')} />
-        </Button>
+        {this.renderFacebookLogin()}
+        {this.renderGoogleLogin()}
       </div>
     );
   }
