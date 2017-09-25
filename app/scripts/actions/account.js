c2d726b732cf01c0a0819e6e78fa80ac8a561ef7
Adam Hola <adam_holman51@gmail.com>
2017-09-25 14:18:10 +0200

    Update to manage Addresses

--- app/scripts/actions/account.js
+++ app/scripts/actions/account.js
@@ -19,9 +19,30 @@ export function accountFetch(): Object {
   };
 }
 
+export function accountAddressFetch(): Object {
+  return {
+    type: AccountConstants.ACCOUNT_ADDRESS_FETCH_REQUEST,
+    payload: {},
+  };
+}
+
 export function accountUpdate(data): Object {
   return {
     type: AccountConstants.ACCOUNT_UPDATE_SUBMIT,
+    payload: data,
+  };
+}
+
+export function accountAddressCreate(data): Object {
+  return {
+    type: AccountConstants.ACCOUNT_ADDRESS_CREATE_SUBMIT,
+    payload: data,
+  };
+}
+
+export function accountAddressDelete(data): Object {
+  return {
+    type: AccountConstants.ACCOUNT_ADDRESS_DELETE,
     payload: data,
   };
 }
