3411a30d1ed485ef1b765ad083af0a4691421aa5
naat_azevedo <natalia.azevedo@printi.com.br>
2017-09-26 14:44:22 -0300

    Sprint043 | Loyalty funnel

--- app/scripts/components/Header/LoyaltyTopbar.jsx
+++ app/scripts/components/Header/LoyaltyTopbar.jsx
@@ -6,8 +6,8 @@ const LoyaltyTopbar = () => (
   <div className="org-loyalty-topbar">
     <div className="container">
       <div className="mol-loyalty-topbar">
-        <div>
-          <p>gold</p>
+        <div className="qrk-type-of-loyalty">
+          <p>club</p>
         </div>
         <div>
           <p>Você é um membro Printi Club Gold! As etiquetas amarelas sinalizam os benefícios que você tem direito. :)</p>
