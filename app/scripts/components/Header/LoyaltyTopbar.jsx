d292fe63780f304a75ab383f42a471e3e8c957ee
naat_azevedo <natalia.azevedo@printi.com.br>
2017-09-26 14:06:56 -0300

    Sprint043 | Lotalty topbar

---
+++ app/scripts/components/Header/LoyaltyTopbar.jsx
@@ -0,0 +1,21 @@
+// @flow
+
+import React from 'react';
+
+const LoyaltyTopbar = () => (
+  <div className="org-loyalty-topbar">
+    <div className="container">
+      <div className="mol-loyalty-topbar">
+        <div>
+          <p>gold</p>
+        </div>
+        <div>
+          <p>Você é um membro Printi Club Gold! As etiquetas amarelas sinalizam os benefícios que você tem direito. :)</p>
+        </div>
+        <div>close</div>
+      </div>
+    </div>
+  </div>
+);
+
+export default LoyaltyTopbar;
