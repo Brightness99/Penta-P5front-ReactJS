55387c5d8a111e521c2b825a1fed9453735ed0ee
naat_azevedo <natalia.azevedo@printi.com.br>
2017-09-27 08:50:45 -0300

    Sprint043 | Layout about loyalty funnel

--- app/scripts/components/Header/LoyaltyTopbar.jsx
+++ app/scripts/components/Header/LoyaltyTopbar.jsx
@@ -1,21 +1,24 @@
 // @flow
 
 import React from 'react';
+import { CloseIcon } from 'components/Icons';
 
-const LoyaltyTopbar = () => (
-  <div className="org-loyalty-topbar">
-    <div className="container">
-      <div className="mol-loyalty-topbar">
-        <div className="qrk-type-of-loyalty">
-          <p>club</p>
+const LoyaltyTopbar = () => {
+  return (
+    <div className="org-loyalty-topbar">
+      <div className="container">
+        <div className="mol-loyalty-topbar">
+          <div>
+            <div className="qrk-type-of-loyalty">
+              <p>club</p>
+            </div>
+            <p>Você é um membro Printi Club Gold! As etiquetas amarelas sinalizam os benefícios que você tem direito. :)</p>
+          </div>
+          <div><CloseIcon /></div>
         </div>
-        <div>
-          <p>Você é um membro Printi Club Gold! As etiquetas amarelas sinalizam os benefícios que você tem direito. :)</p>
-        </div>
-        <div>close</div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default LoyaltyTopbar;
