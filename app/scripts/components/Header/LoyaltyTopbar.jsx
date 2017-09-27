eb79a21ff33041196cd0b1fef25f921743affd0c
naat_azevedo <natalia.azevedo@printi.com.br>
2017-09-27 09:47:54 -0300

    Sprint043 | LogoLoyalty/ LoyaltyContainer/ LoyaltyTopbar

--- app/scripts/components/Header/LoyaltyTopbar.jsx
+++ app/scripts/components/Header/LoyaltyTopbar.jsx
@@ -10,7 +10,7 @@ const LoyaltyTopbar = () => {
         <div className="mol-loyalty-topbar">
           <div>
             <div className="qrk-type-of-loyalty">
-              <p>club</p>
+              <p>Gold</p>
             </div>
             <p>Você é um membro Printi Club Gold! As etiquetas amarelas sinalizam os benefícios que você tem direito. :)</p>
           </div>
---
+++ app/scripts/components/LoyaltyContainer/index.jsx
@@ -0,0 +1,24 @@
+// @flow
+
+import React from 'react';
+
+const LoyaltyContainer = () => {
+
+  return (
+    <div className="container">
+      <div className="org-loyalty-container">
+        <div className="mol-loyalty-container">
+          <div>
+            <div className="qrk-type-of-loyalty">
+              <p>Gold</p>
+            </div>
+            <p>As opções já estão com descontos de até 30% para você!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoyaltyContainer;
+
