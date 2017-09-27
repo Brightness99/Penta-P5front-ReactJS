29da8915d85560a462498e9b47aa3196409d7298
naat_azevedo <natalia.azevedo@printi.com.br>
2017-09-27 10:01:41 -0300

    Sprint043 | LogoLoyalty/ LoyaltyContainer/ LoyaltyTopbar

--- app/scripts/components/LoyaltyContainer/index.jsx
+++ app/scripts/components/LoyaltyContainer/index.jsx
@@ -5,15 +5,13 @@ import React from 'react';
 const LoyaltyContainer = () => {
 
   return (
-    <div className="container">
-      <div className="org-loyalty-container">
-        <div className="mol-loyalty-container">
-          <div>
-            <div className="qrk-type-of-loyalty">
-              <p>Gold</p>
-            </div>
-            <p>As opções já estão com descontos de até 30% para você!</p>
+    <div className="org-loyalty-container">
+      <div className="mol-loyalty-container">
+        <div>
+          <div className="qrk-type-of-loyalty">
+            <p>Gold</p>
           </div>
+          <p>As opções já estão com descontos de até 30% para você!</p>
         </div>
       </div>
     </div>
