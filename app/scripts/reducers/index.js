a75a68726943a12a3ea0a95aaaeed7d9a9c3f18e
Ken <brightnesst99@gmail.com>
2017-09-29 03:52:44 +0900

    Added the policy actions and functions

--- app/scripts/reducers/index.js
+++ app/scripts/reducers/index.js
@@ -13,6 +13,7 @@ import router from './router';
 import user from './user';
 import contactForm from './contact-form';
 import account from './account';
+import policy from './policy';
 
 export default {
   ...app,
@@ -25,4 +26,5 @@ export default {
   ...user,
   ...contactForm,
   ...account,
+  ...policy,
 };
