3cd880af1d92f82cfe1fbbbc49753f9c0737060a
Rodrigo Bacelli <rodrigobac@gmail.com>
2017-09-28 15:34:30 -0300

    Sprint040|ALPHA-749|WIP Fix graphic plant

--- app/scripts/components/LandingPage/index.js
+++ app/scripts/components/LandingPage/index.js
@@ -5,5 +5,6 @@ export BlogBlock from './Blog';
 export BreadcrumbsBlock from './Breadcrumbs';
 export CategoriesCarouselBlock from './CategoriesCarousel';
 export CustomersRelyBlock from './CustomersRely';
+export GraphicPlantBlock from './GraphicPlant';
 export HighlightsBlock from './Highlights';
 export ProductHighlightsBlock from '../../containers/Products/ProductHighlights';
