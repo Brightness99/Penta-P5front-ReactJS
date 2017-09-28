72be19b44381f36156781686688ef28349b162a9
naat_azevedo <natalia.azevedo@printi.com.br>
2017-09-28 09:17:44 -0300

    Sprint043 | fixes LP mobile and desktop

--- app/scripts/containers/Products/BenefitsBlock.jsx
+++ app/scripts/containers/Products/BenefitsBlock.jsx
@@ -2,6 +2,7 @@
 // TODO: Carousel on mobile
 import React from 'react';
 import Logo from 'components/Logo';
+import Slider from 'react-slick';
 import { Link } from 'react-router-dom';
 
 import { CheckIcon, CloseIcon } from 'components/Icons';
@@ -24,7 +25,7 @@ export default class BenefitsBlock extends React.Component {
       <p key={`advantages-${item}`}><i className="checkIcon"><CheckIcon /></i>{item}</p>
     ));
   }
-  //  <h4 className="benefits-title">{advantages.TITLE}</h4>
+  
   renderMobile() {
     const { advantages } = this.props;
 
@@ -33,34 +34,41 @@ export default class BenefitsBlock extends React.Component {
         <div className="container">
           <h4 className="benefits-title">{advantages.TITLE}</h4>
           <div className="container-benefitsCard">
-            <div className="box-benefitsCard">
-              <p className="benefitsCard-title">{advantages.OTHER_ONLINE_PRINTING_COMPANIES}</p>
-              <p><i className="checkIcon"><CheckIcon /></i></p>
-              <p><i className="closeIcon"><CloseIcon /></i></p>
-              <p><i className="checkIcon"><CheckIcon /></i></p>
-              <p><i className="closeIcon"><CloseIcon /></i></p>
-              <p><i className="closeIcon"><CloseIcon /></i></p>
-              <p><i className="closeIcon"><CloseIcon /></i></p>
-              <p><i className="closeIcon"><CloseIcon /></i></p>
-              <p><i className="closeIcon"><CloseIcon /></i></p>
-            </div>
-            <div className="box-benefitsCard">
-              <p className="benefitsCard-title">
-                <Logo />
-              </p>
-              {this.renderListAdvantages()}
-            </div>
-            <div className="box-benefitsCard">
-              <p className="benefitsCard-title">{advantages.TRADITIONAL_PRINTING_COMPANIES}</p>
-              <p><i className="closeIcon"><CloseIcon /></i></p>
-              <p><i className="closeIcon"><CloseIcon /></i></p>
-              <p><i className="closeIcon"><CloseIcon /></i></p>
-              <p><i className="closeIcon"><CloseIcon /></i></p>
-              <p><i className="closeIcon"><CloseIcon /></i></p>
-              <p><i className="checkIcon"><CheckIcon /></i></p>
-              <p><i className="checkIcon"><CheckIcon /></i></p>
-              <p><i className="checkIcon"><CheckIcon /></i></p>
-            </div>
+            <Slider
+              arrows={false}
+              dots={true}
+              dotsClass="atm-carousel-dots"
+              key="product-highlights-slider"
+            >
+              <div className="box-benefitsCard">
+                <p className="benefitsCard-title">{advantages.OTHER_ONLINE_PRINTING_COMPANIES}</p>
+                <p><i className="checkIcon"><CheckIcon /></i></p>
+                <p><i className="closeIcon"><CloseIcon /></i></p>
+                <p><i className="checkIcon"><CheckIcon /></i></p>
+                <p><i className="closeIcon"><CloseIcon /></i></p>
+                <p><i className="closeIcon"><CloseIcon /></i></p>
+                <p><i className="closeIcon"><CloseIcon /></i></p>
+                <p><i className="closeIcon"><CloseIcon /></i></p>
+                <p><i className="closeIcon"><CloseIcon /></i></p>
+              </div>
+              <div className="box-benefitsCard">
+                <p className="benefitsCard-title">
+                  <Logo />
+                </p>
+                {this.renderListAdvantages()}
+              </div>
+              <div className="box-benefitsCard">
+                <p className="benefitsCard-title">{advantages.TRADITIONAL_PRINTING_COMPANIES}</p>
+                <p><i className="closeIcon"><CloseIcon /></i></p>
+                <p><i className="closeIcon"><CloseIcon /></i></p>
+                <p><i className="closeIcon"><CloseIcon /></i></p>
+                <p><i className="closeIcon"><CloseIcon /></i></p>
+                <p><i className="closeIcon"><CloseIcon /></i></p>
+                <p><i className="checkIcon"><CheckIcon /></i></p>
+                <p><i className="checkIcon"><CheckIcon /></i></p>
+                <p><i className="checkIcon"><CheckIcon /></i></p>
+              </div>
+            </Slider>
           </div>
           <sub>{advantages.CHECK_AVAILABLE_PRODUCTS}</sub>
           <div className="benefitsCard-btn">
