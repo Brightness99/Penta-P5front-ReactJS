1d218bfb742bc343fd34267b9b273463d70ea859
naat_azevedo <natalia.azevedo@printi.com.br>
2017-09-29 17:45:05 -0300

    Sprint042 | link to help center

--- app/scripts/containers/Referral/index.jsx
+++ app/scripts/containers/Referral/index.jsx
@@ -103,7 +103,7 @@ export class Referral extends React.Component {
               <div className="container">
                 <div className="atm-infos">
                   <p>Depois de convidar seus amigos, você poderá visualizar o statos das indicações.</p>
-                  <p>Em caso de dúvida, visite a nossa <Link to="#">Central de ajuda</Link></p>
+                  <p>Em caso de dúvida, visite a nossa <Link to="/central-de-ajuda">Central de ajuda</Link></p>
                 </div>
               </div>
             </div>
