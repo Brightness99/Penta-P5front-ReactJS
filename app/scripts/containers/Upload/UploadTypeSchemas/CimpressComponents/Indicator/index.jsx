3a0826f6385a17239b10db0f86935c01bf87bbea
Rodrigo Bacelli <rodrigobac@gmail.com>
2017-10-02 16:23:46 -0300

    :shirt: Clear linter warnings

--- app/scripts/containers/Upload/UploadTypeSchemas/CimpressComponents/Indicator/index.jsx
+++ app/scripts/containers/Upload/UploadTypeSchemas/CimpressComponents/Indicator/index.jsx
@@ -9,23 +9,21 @@ type Props = {
   content: string,
 };
 
-const Indicator = (props: Props) => {
-  return (
-    <div className={`dcl-canvas__info-modal dcl-canvas__info-modal--${props.type}`}>
-      <div className="dcl-header-text">
-        {props.type !== 'trim' && <EyeIcon />}
-        {props.title}
-      </div>
-      <div className="dcl-margin-indicator-positioner">
-        <div className="dcl-margin-indicator-message">
-          {props.content}
-        </div>
-        <div className="dcl-pointer-line dcl-pointer-line__bg" />
-        <div className="dcl-pointer-line dcl-pointer-line__fg" />
-        <div className="dcl-pointer-circle" />
-      </div>
+const Indicator = (props: Props) => (
+  <div className={`dcl-canvas__info-modal dcl-canvas__info-modal--${props.type}`}>
+    <div className="dcl-header-text">
+      {props.type !== 'trim' && <EyeIcon />}
+      {props.title}
     </div>
-  );
-};
+    <div className="dcl-margin-indicator-positioner">
+      <div className="dcl-margin-indicator-message">
+        {props.content}
+      </div>
+      <div className="dcl-pointer-line dcl-pointer-line__bg" />
+      <div className="dcl-pointer-line dcl-pointer-line__fg" />
+      <div className="dcl-pointer-circle" />
+    </div>
+  </div>
+);
 
 export default Indicator;
