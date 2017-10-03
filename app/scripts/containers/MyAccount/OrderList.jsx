03f5fc035a72306ae6a2ed3c87767ad8c9dffbe3
brightnesst99 <brightnesst99@gmail.com>
2017-10-03 19:45:15 -0300

    Sprint043|WIP fix order list page

--- app/scripts/containers/MyAccount/OrderList.jsx
+++ app/scripts/containers/MyAccount/OrderList.jsx
@@ -1,43 +1,66 @@
 // @flow
 import React from 'react';
 import Breadcrumbs from 'components/Breadcrumbs';
-import moment from 'moment';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { shouldComponentUpdate, isMobile } from 'utils/helpers';
 import Loading from 'components/Loading';
-import { accountOrderFetch } from 'actions';
-import { CodeBar, Receipt, ExclamationMark, DocumentDownload, Clipboard, CardsIcon, RepurchaseIcon, WatchIcon } from 'components/Icons';
-import cx from 'classnames';
+import Tooltip from 'components/Tooltipster';
+import { accountOrdersFetch } from 'actions';
+import { CodeBar, Receipt, ImageFileIcon, NFIcon, Clipboard } from 'components/Icons';
+import { IntlDate, IntlMoney } from 'components/Intl';
 
 type Props = {
   screenSize: string,
   account: {},
+  orders: {},
   dispatch: () => {},
 };
 
+type State = {
+  page: number,
+};
+
 export class OrderList extends React.Component {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      page: 1,
+    };
+  }
 
   shouldComponentUpdate = shouldComponentUpdate;
 
   componentDidMount() {
     const { dispatch } = this.props;
+    const { page } = this.state;
 
-    dispatch(accountOrderFetch());
+    dispatch(accountOrdersFetch(page));
   }
 
-  static defaultProps = {
-    screenSize: 'xs',
-  };
+  componentDidUpdate(prevProps, prevState) {
+    const { dispatch } = this.props;
+    const { page } = this.state;
+
+    if (prevState.page !== page) {
+      dispatch(accountOrdersFetch(page));
+    }
+  }
 
   static props: Props;
-  state: State;
 
-  showDetails = () => {
-    
-  }
+  static state: State;
 
-  renderItems() {
+  handleLoadMore = () => {
+    const { page } = this.state;
+
+    this.setState({
+      page: page + 1,
+    });
+  };
+
+  renderItemsO() {
     const { account: { orders }, screenSize } = this.props;
 
     if (orders.list.length <= 0) {
@@ -91,55 +114,119 @@ export class OrderList extends React.Component {
       ));
     }
 
-    return orders.list.map((item) => (
-      <div className={cx('box-detailsOrder', {
-        delivered: item.status_value === 'Em andamento',
-        pendingPayment: item.status_value === 'Aguardando pagamento',
-        inTransport: item.status_value === ''
-      })} key={item.info.id}>
-        <div className="box-firstPart">
-          <div>
-            <p>{item.info.id}</p>
+    return null;
+  }
+
+  renderActions(order) {
+    return Object.keys(order.actions)
+      .filter((key) => key !== 'details' && order.actions[key].enabled)
+      .slice(0, 2)
+      .map((key) => {
+        let icon = null;
+
+        switch (key) {
+          case 'boleto':
+            icon = <CodeBar />;
+            break;
+          case 'invoice':
+            icon = <NFIcon />;
+            break;
+          case 'upload':
+            icon = <ImageFileIcon />;
+            break;
+          default:
+            icon = null;
+            break;
+        }
+
+        return (
+          <Tooltip
+            key={key}
+            text={order.actions[key].label}
+          >
+            <Link to={order.actions[key].path || ''} className="atm-transparent-button">
+              {icon}
+            </Link>
+          </Tooltip>
+        );
+      });
+  }
+
+  renderMobile() {
+    return (
+      <div>123</div>
+    );
+  }
+
+  renderDesktop() {
+    const { orders } = this.props;
+    const { page } = this.state;
+
+    return (
+      <div className="org-opinions-desktop">
+        <ul className="org-opinions-header">
+          <li>Nº DO PEDIDO</li>
+          <li>REALIZADO EM</li>
+          <li>STATUS</li>
+          <li>AÇÕES</li>
+        </ul>
+        <ul className="org-opinions-list">
+          {orders.list.map((order) => (
+            <li
+              key={order.info.id}
+              className={order.status_class}
+            >
+              <div className="org-opinions-list-data">
+                <div>{order.info.id}</div>
+                <div><IntlDate>{order.info.created_at}</IntlDate></div>
+                <div><i />{order.status_value}</div>
+                <div>
+                  {this.renderActions(order)}
+                </div>
+                <div>{order.actions.details.enabled && <Link to={`/minha-conta/pedidos/${order.info.id}`}>{order.actions.details.label}</Link>}</div>
+              </div>
+              <div className="org-opinions-list-expand">
+                <div>
+                  Itens do pedido
+                  <span>{order.items_label}</span>
+                </div>
+                <div>
+                  Valor total
+                  <IntlMoney>{order.info.total_price}</IntlMoney>
+                </div>
+              </div>
+            </li>
+          ))}
+          {orders.isLoaded && orders.isRunning && <Loading />}
+        </ul>
+        {page < Math.ceil(orders.total_count / 10) &&
+          <div className="atm-opinions-load-more">
+            <button
+              className="atm-button-transparent"
+              onClick={this.handleLoadMore}
+            >
+              Carregar mais pedidos
+            </button>
           </div>
-          <div>
-            <p>{item.date_value}</p>
-          </div>
-          <div className="flagOrder">
-            <p>{item.status_value}</p>
-          </div>
-          <div className="box-icons">
-            <div className="icons">
-              <Link to="#" className="btn-icons">
-                {item.actions.invoice.label}
-              </Link>
-              <Link to="#" className="btn-icons">
-                {item.actions.upload.label}
-              </Link>
-            </div>
-            <Link to={`/minha-conta/pedidos/${item.info.id}`} className="icons align-text" onClick={this.showDetails}>{item.actions.details.label}</Link>
-          </div>
-        </div>
-        <div className="box-secondPart">
-          <div className="box-images">
-            <img src={require('assets/media/images/imgteste-produto.jpg')} alt="Produto" />
-            <img src={require('assets/media/images/imgteste-produto2.jpg')} alt="Produto" />
-            <img src={require('assets/media/images/imgteste-produto3.jpg')} alt="Produto" />
-          </div>
-          <div>
-            <p className="title-secondPart">Itens do pedido</p>
-            <p className="txt-secondPart">{item.items_label}</p>
-          </div>
-          <div>
-            <p className="title-secondPart">Valor total</p>
-            <p className="txt-secondPart">R$ {item.info.total_price}</p>
-          </div>
-        </div>
+        }
       </div>
-    ));
+    );
+  }
+
+  renderItems() {
+    const { orders, screenSize } = this.props;
+
+    console.log('isRunning', orders.isRunning, 'isLoaded', orders.isLoaded);
+
+    if (!orders.isLoaded && orders.isRunning) {
+      return <Loading />;
+    }
+
+    return isMobile(screenSize) ? this.renderMobile() : this.renderDesktop();
   }
 
   render() {
-    const { account: { orders }, screenSize } = this.props;
+    const { screenSize } = this.props;
 
     const breadcrumb = [
       {
@@ -160,13 +247,7 @@ export class OrderList extends React.Component {
         <h2>Minha conta</h2>
         <h3 className="subtitle-myorder">Meus pedidos</h3>
         <p className="legend-myorder">Acompanhe os status do seus pedidos</p>
-        {!isMobile(screenSize) && (<ul className="box-tableOrder">
-          <li>Nº do pedido</li>
-          <li>Realizado em</li>
-          <li>Status</li>
-          <li>Ações</li>
-        </ul>)}
-        {!orders.isLoaded || orders.isRunning ? <Loading /> : this.renderItems()}
+        {this.renderItems()}
       </div>
     );
   }
@@ -175,7 +256,7 @@ export class OrderList extends React.Component {
 function mapStateToProps(state) {
   return {
     screenSize: state.app.screenSize,
-    account: state.account,
+    orders: state.account.orders,
   };
 }
 
