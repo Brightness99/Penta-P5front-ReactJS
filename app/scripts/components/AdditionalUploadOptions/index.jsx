c2417db3c2e45e9a5495d6422792d5b84add6e5f
Boikov <boikovdmitriy@mail.ru>
2017-10-11 00:41:53 +0400

    Sprint 043 | ALPHA-973 | added selection logic to first step

--- app/scripts/components/AdditionalUploadOptions/index.jsx
+++ app/scripts/components/AdditionalUploadOptions/index.jsx
@@ -4,22 +4,47 @@ import React from 'react';
 import AdditionalOption from './AdditionalUploadOption';
 
 type Props = {
-    items: []
+    items: [],
+    handleOptionsChanged: (options: {}) => void
+}
+
+type State = {
+  selectedOptions: []
 }
 
 export default class AdditionalUploadOptions extends React.Component {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      selectedOptions: [],
+    };
+  }
+
   props: Props;
+  state: State;
+
+  handleSelectedOption(option, title) {
+    const { selectedOptions } = this.state;
+    const { items, handleOptionsChanged } = this.props;
+    const newSelectedOptions = [...selectedOptions.filter(x => x.title !== title), { title, option }];
+    this.setState({
+      selectedOptions: newSelectedOptions,
+    });
+    if (newSelectedOptions.length === items.length && typeof handleOptionsChanged === 'function') {
+      handleOptionsChanged(newSelectedOptions);
+    }
+  }
 
   renderList() {
     const { items } = this.props;
     return items.map(
       (additionalOption) => (
         <AdditionalOption
-          key={`${new Date()}-${additionalOption.title}`}
+          key={additionalOption.title}
           title={additionalOption.title}
           video={additionalOption.video}
           options={additionalOption.options}
-          handleOptionSelected={(option) => console.log(option)}
+          handleOptionSelected={(option) => this.handleSelectedOption(option, additionalOption.title)}
         />
       )
     );
