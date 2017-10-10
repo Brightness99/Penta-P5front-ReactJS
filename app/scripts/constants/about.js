172e55222c7172213c221e5d081360622eb8412f
Ken <brightnesst99@gmail.com>
2017-10-10 11:11:39 +0900

    integrated the API of about printi page

---
+++ app/scripts/constants/about.js
@@ -0,0 +1,19 @@
+// @flow
+
+/**
+ * @module Constants/About
+ * @desc About Constants
+ */
+
+import keyMirror from 'fbjs/lib/keyMirror';
+
+/**
+ * @constant {Object} AboutConstants
+ * @memberof Constants
+ */
+export const AboutConstants = keyMirror({
+  ABOUT_FETCH_REQUEST: undefined,
+  ABOUT_FETCH_SUCCESS: undefined,
+  ABOUT_FETCH_FAILURE: undefined,
+  ABOUT_FETCH_CANCEL: undefined,
+});
