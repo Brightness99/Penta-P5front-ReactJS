88a12467bab749c521f5c0caa842d224c61e46ee
Boikov <boikovdmitriy@mail.ru>
2017-10-10 23:48:12 +0400

    Sprint 043 | ALPHA-973 | added fileUpload block

---
+++ app/scripts/components/UploadTypeSchemes/index.jsx
@@ -0,0 +1,39 @@
+// @flow
+import React from 'react';
+import UploadFile from 'components/UploadFile';
+
+type Props = {
+  multipleFiles: boolean,
+  handleUploadFile: (file: {}) => void,
+  isLoadStarted: boolean,
+  uploadFileProgresses: { fileName: string, progress: number }
+}
+
+export default class UploadTypeSchemes extends React.Component {
+  static defaultProps = {
+    multipleFiles: false,
+    uploadFileProgresses: [],
+  };
+
+  props: Props;
+
+  renderUploadFiles() {
+    const { multipleFiles } = this.props;
+    if (multipleFiles) {
+      return [<UploadFile key="arte1" />, <UploadFile key="arte2" />];
+    }
+    return (
+      <UploadFile key="arte1" />
+    );
+  }
+
+  render() {
+    return (
+      <section className="upload-type-schemes-container">
+        {
+          this.renderUploadFiles()
+        }
+      </section>
+    );
+  }
+}
