364bd0682eff3b1066f10a7f20724cc0175743c6
brightnesst99 <brightnesst99@gmail.com>
2017-10-13 18:33:49 -0300

    Sprint043|ALPHA-981|Update FileMount route and fix conflicts

--- app/scripts/containers/App.jsx
+++ app/scripts/containers/App.jsx
@@ -94,7 +94,7 @@ export class App extends React.Component {
                       <Route path="/produtos-:slug" component={Products} />
                       <Route path="/login-cadastro" component={Authentication} />
                       <Route path="/minha-conta" component={MyAccount} />
-                      <Route path="/montagem-de-arquivos" component={FileMount} />
+                      <Route path="/montagem-do-arquivo" component={FileMount} />
                       <Route path="/guia-de-impressao" component={PrintGuide} />
                       <Route path="/central-de-ajuda" component={HelpCenterPage} />
                       <Route path="/politica-de-privacidade" component={PrivacyPolicy} />
