00493e3788b02f5c82df46a3ab985f2134d77eab
Boikov <boikovdmitriy@mail.ru>
2017-10-17 20:18:33 +0400

    Sprint 043 | ALPHA-973 | fixed upload file component

--- app/scripts/components/FileFormatIcon/index.jsx
+++ app/scripts/components/FileFormatIcon/index.jsx
@@ -2,8 +2,8 @@
 import React from 'react';
 import { CalendarIcon, HelpIcon, FileIcon } from 'components/Icons';
 
-const FileFormatIcon = (props: { title: string }) => (
-  <section className="file-format-icon-container">
+const FileFormatIcon = (props: { title: string, className: string }) => (
+  <section className={`file-format-icon-container ${props.className}`}>
     <FileIcon />
     <span>{props.title}</span>
   </section>
