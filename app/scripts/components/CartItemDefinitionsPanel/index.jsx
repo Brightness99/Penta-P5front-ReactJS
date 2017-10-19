0218a5d3622ca9c44357f4e6e90dd64d865e8a91
Boikov <boikovdmitriy@mail.ru>
2017-10-19 18:37:05 +0400

    Sprint 043 | ALPHA-973 | fixed cartItem with 2 parts of item

--- app/scripts/components/CartItemDefinitionsPanel/index.jsx
+++ app/scripts/components/CartItemDefinitionsPanel/index.jsx
@@ -2,25 +2,35 @@
 import React from 'react';
 
 type Props = {
-  options: Array<{field: string, value: string}>
+  parts: Array<{
+    name: string,
+    options: Array<{ field: string, value: string }>
+  }>
 };
 
 export default class CartItemDefinitionsPanel extends React.Component {
   props: Props;
 
+  renderParts() {
+    const { parts } = this.props;
+    return parts.map(x => (
+      <section key={x.name}>
+        {parts.length > 1 && <h5>{x.name}</h5>}
+        <ul>
+          {
+            x.options.map(y => <li key={y.field}><span><strong>{`${y.field}: `}</strong>{y.value}</span></li>)
+          }
+        </ul>
+      </section>));
+  }
+
   render() {
-    const { options } = this.props;
     return (
       <aside className="cart-item-definitions-container">
         <h4>Resumo do produto</h4>
         <section className="options">
-          <ul>
-            {
-              options.map(x => <li key={x.field}><span><strong>{`${x.field}: `}</strong>{x.value}</span></li>)
-            }
-          </ul>
+          {this.renderParts()}
         </section>
-        <hr />
         <section className="sub-total">sub-tota</section>
         <hr />
         <section className="additional-options">additional-options</section>
