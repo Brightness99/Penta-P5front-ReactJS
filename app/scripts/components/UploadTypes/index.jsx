0a71db91bd83e216072073058eb1809b9c37d4bf
Boikov <boikovdmitriy@mail.ru>
2017-10-23 15:17:14 +0400

    Sprint 043 | ALPHA-973 | refactoring

--- app/scripts/components/UploadTypes/index.jsx
+++ app/scripts/components/UploadTypes/index.jsx
@@ -1,1 +1,29 @@
-export NormalUploadType from './NormalUploadType';
+// @flow
+import React from 'react';
+import NormalUploadType from './NormalUploadType';
+import CanvasSchema from './Canvas';
+import SkuSceneSchema from './SkuScene';
+
+type Props = {
+  uploadType: string,
+  selectedStrategy: number,
+}
+
+const UploadTypes = ({ uploadType, selectedStrategy }: Props) => {
+  switch (uploadType) {
+    case 'canvas':
+      return <CanvasSchema />;
+    case 'sku_scene':
+      return <SkuSceneSchema />;
+    default:
+      return (
+        <NormalUploadType
+          uploadTwoFiles={selectedStrategy === 4}
+          multipleFiles={selectedStrategy === 5}
+          handleUploadFile={this.handleUploadFile}
+          handleRemoveFile={this.handleRemoveFile}
+        />);
+  }
+};
+
+export default UploadTypes;
