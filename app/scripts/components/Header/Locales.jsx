8ea1b8a4a6a87ddc0af8d87698fc6b3c46adfb9f
Rodrigo Pietro Bacelli <rodrigo.bacelli@printi.com.br>
2017-10-24 20:13:18 -0200

    Sprint044|WIP Add dropdown for locale selection

--- app/scripts/components/Header/Locales.jsx
+++ app/scripts/components/Header/Locales.jsx
@@ -1,46 +1,39 @@
 // @flow
 
 import React from 'react';
-import { shouldComponentUpdate } from 'utils/helpers';
 import { NavLink } from 'react-router-dom';
 
-
 type Props = {
+  currentCountry: string,
 };
 
-export default class HeaderModels extends React.Component {
-  shouldComponentUpdate = shouldComponentUpdate;
+const locales = {
+  US: {
+    name: 'United States',
+    flag: '',
+    link: 'http://react.printi.com',
+  },
+  BR: {
+    name: 'Brasil',
+    flag: '',
+    link: 'http://react.printi.com.br',
+  },
+};
 
-  static props: Props;
+const LocaleChange = (props: Props) => {
+  const { currentCountry } = props;
 
-  render() {
-    return (
-      <ul className="mol-topbar-modelList">
-        <li><NavLink to="/modelos-de-cardapio-de-papel">Modelos de Cardápio de Papel</NavLink></li>
-        <li><NavLink to="/modelos-de-cartao-de-visita">Modelos de Cartão de Visita</NavLink></li>
-        <li><NavLink to="/modelos-de-identificador-de-porta">Modelos de Identificador de Porta</NavLink></li>
-        <li><NavLink to="/modelos-de-papel-timbrado">Modelos de Papel Timbrado</NavLink></li>
-        <li><NavLink to="/modelos-de-flyer">Modelos de Flyer</NavLink></li>
-        <li><NavLink to="/modelos-de-adesivo">Modelos de Adesivo</NavLink></li>
-        <li><NavLink to="/modelos-de-folhetos">Modelos de Folhetos</NavLink></li>
-        <li><NavLink to="/modelos-de-panfletos">Modelos de Panfletos</NavLink></li>
-        <li><NavLink to="/modelos-de-banner">Modelos de Banner</NavLink></li>
-        <li><NavLink to="/modelos-de-banner-l">Modelos de Banner L</NavLink></li>
-        <li><NavLink to="/modelos-de-folder">Modelos de Folder</NavLink></li>
-        <li><NavLink to="/modelos-de-receituario">Modelos de Receituário</NavLink></li>
-        <li><NavLink to="/modelos-de-banner-roll-up">Modelos de Banner Roll Up</NavLink></li>
-        <li><NavLink to="/modelos-de-cartaz">Modelos de Cartaz</NavLink></li>
-        <li><NavLink to="/modelos-de-cartao-fidelidade">Modelos de Cartão Fidelidade</NavLink></li>
-        <li><NavLink to="/modelos-de-cartao-postal">Modelos de Cartão Postal</NavLink></li>
-        <li><NavLink to="/modelos-de-marca-pagina">Modelos de Marca Página</NavLink></li>
-        <li><NavLink to="/modelos-de-pasta">Modelos de Pasta</NavLink></li>
-        <li><NavLink to="/modelos-de-banner-x">Modelos de Banner X</NavLink></li>
-        <li><NavLink to="/modelos-de-convite">Modelos de Convite</NavLink></li>
-        <li><NavLink to="/modelos-de-filipeta">Modelos de Filipeta</NavLink></li>
-        <li><NavLink to="/modelos-de-posters">Modelos de Pôster</NavLink></li>
-        <li><NavLink to="/modelos-de-bloco-de-notas">Modelos de Bloco de Notas</NavLink></li>
-        <li><NavLink to="/modelos-de-cardapio-de-plastico">Modelos de Cardápio de Plástico</NavLink></li>
-      </ul>
-    );
-  }
-}
+  return (
+    <ul className="mol-topbar-locales">
+      {
+        Object.keys(locales)
+        .filter((country) => country !== currentCountry)
+        .map((country) => (
+          <li key={country}><NavLink to={locales[country].link}>{locales[country].name}</NavLink></li>
+        ))
+      }
+    </ul>
+  );
+};
+
+export default LocaleChange;
--- app/scripts/components/Header/Topbar.jsx
+++ app/scripts/components/Header/Topbar.jsx
@@ -3,13 +3,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { HeartIcon, HelpCircleIcon, AngleDownIcon } from 'components/Icons';
+import Locales from './Locales';
 import Models from './Models';
 
 type Props = {
+  locale: {},
   handleClose: () => {},
 };
 
 const Topbar = (props: Props) => {
+  const { locale } = props;
+
   const handleAction = (ev) => {
     const { handleClose } = props;
 
@@ -21,6 +25,12 @@ const Topbar = (props: Props) => {
   return (
     <div className="org-topbar">
       <div className="container">
+        <div className="mol-topbar-locales">
+          <button onMouseOver={handleAction} className="atm-topbar-link">
+            {locale.COUNTRY}
+          </button>
+          <Locales currentCountry={locale.COUNTRY_CODE} />
+        </div>
         <NavLink className="atm-topbar-link atm-topbar-referral" to="/indique-a-printi">
           Indique a Printi <HeartIcon />
         </NavLink>
