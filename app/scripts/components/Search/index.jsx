fee12bde6d0e34a7a04a674c7be252edaca93f07
Boikov <boikovdmitriy@mail.ru>
2017-10-24 15:27:26 +0400

    Sprint 044 | AECOM-86 | added working search bar on "buscar" page and working without page reloading

--- app/scripts/components/Search/index.jsx
+++ app/scripts/components/Search/index.jsx
@@ -1,9 +1,12 @@
 // @flow
 
 import React from 'react';
+import { push } from 'modules/ReduxRouter';
 import { connect } from 'react-redux';
 import Breadcrumbs from 'components/Breadcrumbs';
 import { isMobile, shouldComponentUpdate } from 'utils/helpers';
+
+declare var google: {};
 
 type Props = {
   app: {},
@@ -16,34 +19,90 @@ type Props = {
   match: {},
 };
 
+
 export class Search extends React.Component {
   shouldComponentUpdate = shouldComponentUpdate;
-  
+
   componentDidMount() {
     this.handleCSE();
   }
 
-  componentWillReceiveProps(nextProps) {
-    console.log(nextProps);
-    
-    if (nextProps.router.location.search !== this.props.router.location.search) {
-      document.querySelector('#gcse_printi').parentElement.removeChild(document.querySelector('#gcse_printi'));
-      this.handleCSE();
+  componentWillReceiveProps(newProps) {
+    const newSearch = newProps.router.location.search;
+    const search = this.props.router.location.search;
+    if (newSearch !== search) {
+      this.setSearchQuery(newSearch.replace('?q=', ''));
     }
+  }
+
+  componentWillUnmount() {
+    this.removeScript();
   }
 
   static props: Props;
 
+  removeScript() {
+    const script = document.getElementById('gcse_printi');
+    script.remove();
+    /* TODO: If someone have problem with google API, please told me. @Dmitriy Boikov
+     * It's dirty hack for using without reload page. Without it would be error If you leave the page and come back*/
+    delete window.google;
+    delete window.__gcse;
+  }
+
   handleCSE = () => {
+    window.__gcse = {
+      parsetags: 'explicit',
+      callback: () => {
+        if (document.readyState === 'complete') {
+          this.renderSearchBox();
+        } else {
+          google.setOnLoadCallback(() => {
+            this.renderSearchBox();
+          }, true);
+        }
+      },
+    };
+
     const cx = '016989531617499423574:cwcw4nxwj10';
     const gcse = document.createElement('script');
     gcse.id = 'gcse_printi';
     gcse.type = 'text/javascript';
     gcse.async = true;
     gcse.src = `${document.location.protocol === 'https:' ? 'https:' : 'http:'}//www.google.com/cse/cse.js?cx=${cx}`;
-    const s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
+    document.body.insertBefore(gcse, document.body.firstChild);
   };
+
+  renderSearchBox() {
+    google.search.cse.element.render({
+      div: 'search-box',
+      tag: 'searchbox',
+      gname: 'search-site',
+    }, {
+      div: 'search-result',
+      tag: 'searchresults',
+      gname: 'search-site',
+    });
+    setTimeout(this.subscribeToQueryChanging, 1000);
+  }
+
+  subscribeToQueryChanging = () => {
+    document.getElementsByClassName('gsc-search-button-v2')[0].addEventListener('click', this.queryChanged);
+    document.getElementById('gsc-i-id1').addEventListener('keyup', (e) => {
+      if (e.keyCode === 13) {
+        this.queryChanged();
+      }
+    });
+  };
+
+  queryChanged = () => {
+    const value = document.getElementById('gsc-i-id1').value;
+    push(`./buscar?q=${value}`);
+  };
+
+  setSearchQuery(query: string) {
+    google.search.cse.element.getElement('search-site').execute(query);
+  }
 
   render() {
     const { app: { screenSize } } = this.props;
@@ -61,16 +120,8 @@ export class Search extends React.Component {
         <div className="container">
           {!isMobile(screenSize) && <Breadcrumbs links={breadcrumb} />}
           <div className="org-search-page">
-            {React.createElement('gcse:searchbox-only', {
-              resultsUrl: '/buscar',
-              queryParameterName: 'q',
-              autoCompleteMatchType: 'any',
-              enableAutoComplete: 'true',
-            })}
-            {React.createElement('gcse:searchresults-only', {
-              linkTarget: '_self',
-              queryParameterName: 'q',
-            })}
+            <div id="search-box" />
+            <div id="search-result" />
           </div>
         </div>
       </section>
