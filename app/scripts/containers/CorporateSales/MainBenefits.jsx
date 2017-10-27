f6f63dc6ae4085959fd163a5cc82ee52206d168b
Anton Valov <anton.valov.o@gmail.com>
2017-10-27 17:14:57 +0300

    AECOM-80::Implement Corporate Sales page - In Review;
    Fixes for mobile banner;
    Fixes for desktop benefits;

--- app/scripts/containers/CorporateSales/MainBenefits.jsx
+++ app/scripts/containers/CorporateSales/MainBenefits.jsx
@@ -3,40 +3,90 @@
 import React from 'react';
 import Slider from 'react-slick';
 import { InvoiceIcon, SpecialProjectsIcon, AccountManagerIcon } from 'components/Icons';
+import { getScreenSize } from '../../utils/helpers';
 
-export class MainBenefits extends React.Component {
+const benefits = [
+  {
+    key: 'benefit-1',
+    title: 'Gerente de contas',
+    text: `Ficou com dúvida na hora de fazer seu pedido? Fale com um de nossos
+            executivos especializados e tenha um atendimento personalizado.`,
+    icon: AccountManagerIcon,
+  },
+  {
+    key: 'benefit-2',
+    title: 'Projetos especiais',
+    text: `Não encontrou algum produto no site? No atendimento exclusivo, você
+            também pode fazer projetos especiais de acordo com a sua demanda.`,
+    icon: SpecialProjectsIcon,
+  },
+  {
+    key: 'benefit-3',
+    title: 'Pagamento faturado',
+    text: `Opção de pagamento perfeita para empresas. Fature seu pagamento e tenha
+            facilidade e comodidade na realização dos pedidos.`,
+    icon: InvoiceIcon,
+  },
+];
+
+export class MainBenefits extends React.PureComponent {
+  renderDesktop = () => (
+    <section className="boxes-our-benefits">
+      {
+        benefits.map(benefit => {
+          const Icon = benefit.icon;
+          return (
+            <section key={benefit.key} className="box-our-benefits">
+              <Icon />
+              <p className="title-our-benefits">
+                {benefit.title}
+              </p>
+              <p className="text-our-benefits">
+                {benefit.text}
+              </p>
+            </section>
+          );
+        })
+      }
+    </section>
+  );
+
+  renderMobile = () => (
+    <section className="boxes-our-benefits mobile">
+      <Slider
+        className="org-component-banners"
+        arrows={false}
+        dots={true}
+        autoplay={true}
+      >
+        {
+          benefits.map(benefit => {
+            const Icon = benefit.icon;
+            return (
+              <section key={benefit.key} className="box-our-benefits">
+                <Icon />
+                <p className="title-our-benefits">
+                  {benefit.title}
+                </p>
+                <p className="text-our-benefits">
+                  {benefit.text}
+                </p>
+              </section>
+            );
+          })
+        }
+      </Slider>
+    </section>
+  );
+
+
   render() {
+    const hasSlider = ['xs', 'is', 'sm', 'ix'].includes(getScreenSize());
     return (
       <section className="container-main-benefits">
         <section className="container">
           <h4 className="title-corporate-sales">Principais Benefícios</h4>
-          <section className="boxes-our-benefits">
-            <Slider
-              className="org-component-banners"
-              arrows={false}
-              dots={true}
-              autoplay={true}
-            >
-              <section className="box-our-benefits">
-                <AccountManagerIcon />
-                <p className="title-our-benefits">Gerente de contas</p>
-                <p className="text-our-benefits">Ficou com dúvida na hora de fazer seu pedido? Fale com um de nossos
-                  executivos especializados e tenha um atendimento personalizado.</p>
-              </section>
-              <section className="box-our-benefits">
-                <SpecialProjectsIcon />
-                <p className="title-our-benefits">Projetos especiais</p>
-                <p className="text-our-benefits">Não encontrou algum produto no site? No atendimento exclusivo, você
-                  também pode fazer projetos especiais de acordo com a sua demanda. </p>
-              </section>
-              <section className="box-our-benefits">
-                <InvoiceIcon />
-                <p className="title-our-benefits">Pagamento faturado</p>
-                <p className="text-our-benefits">Opção de pagamento perfeita para empresas. Fature seu pagamento e tenha
-                  facilidade e comodidade na realização dos pedidos.</p>
-              </section>
-            </Slider>
-          </section>
+          {hasSlider ? this.renderMobile() : this.renderDesktop()}
         </section>
       </section>
     );
