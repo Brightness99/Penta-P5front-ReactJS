c72b1828ce10837de915dd56526ee6e101813f51
Anton Valov <anton.valov.o@gmail.com>
2017-11-01 17:43:53 +0300

    Sprint044/AECOM-79::Referral page - In Review;
    * Integrated API for referral;
    * Added copy clipboard util function;
    * Added share via email/copy voucher;
    * Added share via facebook (need to check keys);
    * Refactoring;
    * Minor fixes;

--- app/scripts/containers/Referral/index.jsx
+++ app/scripts/containers/Referral/index.jsx
@@ -1,53 +1,91 @@
 // @flow
 
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import * as ReferralActions from 'actions/referral';
 import { isMobile, shouldComponentUpdate } from 'utils/helpers';
+import { copyElementContentToClipboard } from 'utils/copy-util';
+import { socialLoginSettingsFetch } from 'actions';
 
+import ShareCode from 'components/ShareCodeModal';
 import Breadcrumbs from 'components/Breadcrumbs';
 import Modal from 'components/Modal';
+import TooltipEnhancer from 'components/TooltipEnhancer';
+import Loading from 'components/Loading/index';
 import { Input } from 'quarks/Inputs';
 
-import ShareCode from './Modals/ShareCode';
 
 type Props = {
   app: AppStore,
-  router: RouterStore,
-  dispatch: () => {},
-  children: any,
+  sendReferralRequest: (customerId: string, emails: Array<string>) => void,
+  customerInfo: UserCustomerInfoType,
+  referral: ReferralType,
+  socialLoginSettingsFetch: () => void,
+  facebook: {
+    status: string,
+    credentials: {
+      app_id: number | string,
+      secret_key: string,
+    },
+  },
+  language: string,
 };
 
 type State = {
-  modal: {
-    type: string,
-    isOpen: boolean,
-    itemId: string,
-  },
+  showShareModal: boolean,
+  showCopiedTooltip: boolean,
 };
 
-export class Referral extends React.Component {
+export class Referral extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    this.state = { showShareModal: true };
+    this.state = {
+      showShareModal: false,
+      showCopiedTooltip: false,
+    };
   }
 
   shouldComponentUpdate = shouldComponentUpdate;
 
+  componentDidMount() {
+    this.props.socialLoginSettingsFetch();
+  }
+
   static props: Props;
   static state: State;
 
-  handleCloseModal = () => {
-    this.setState({ showShareModal: false });
+  handleShowingModal = () => {
+    this.setState({ showShareModal: !this.state.showShareModal });
   };
 
-  handleShowingModal = () => {
-    this.setState({ showSharetModal: !this.state.showShareModal });
+  handleShowingTooltip = () => {
+    this.setState({ showCopiedTooltip: !this.state.showCopiedTooltip });
+  };
+
+  handleSubmit = (e: Event) => {
+    e.preventDefault();
+    this.handleShowingModal();
+  };
+
+  handleCopy = () => {
+    const isSuccessful = copyElementContentToClipboard('promo_code');
+    const { customerInfo: { voucher_id, voucher_name } = {} } = this.props;
+    if (isSuccessful && Boolean(voucher_name || voucher_id)) {
+      this.handleShowingTooltip();
+    }
   };
 
   render() {
-    const { app: { screenSize } } = this.props;
+    const {
+      app: { screenSize }, customerInfo = {},
+      referral, facebook, language,
+    } = this.props;
+    const { showShareModal, showCopiedTooltip } = this.state;
+    const { voucher_id, voucher_name } = customerInfo;
+    const isLoggedIn = customerInfo.id;
     const breadcrumb = [
       {
         title: 'Home',
@@ -57,71 +95,123 @@ export class Referral extends React.Component {
         title: 'Indique a Printi',
       },
     ];
+    const { isRunning } = referral;
+
+    if (isRunning || customerInfo.isRunning) {
+      return <Loading />;
+    }
+
+    const EnhancedCopyButton = TooltipEnhancer(showCopiedTooltip)(() =>
+      <button type="button" className="atm-link-copy" onClick={this.handleCopy}>
+        {'copiar'}
+      </button>
+    );
+
     return (
-      <div>
-        { this.state.showShareModal &&
-          <div className="modal-referral">
-            <Modal handleCloseModal={this.handleCloseModal}>
-              <ShareCode />
+      <section>
+        {
+          showShareModal &&
+          <section className="modal-referral">
+            <Modal handleCloseModal={this.handleShowingModal}>
+              <ShareCode
+                customerId={customerInfo.id}
+                sendReferralRequest={this.props.sendReferralRequest}
+                handleCloseModal={this.handleShowingModal}
+                voucher={{ voucher_id, voucher_name }}
+                facebook={facebook}
+                language={language}
+                screenSize={screenSize}
+              />
             </Modal>
-          </div>
+          </section>
         }
         <section className="bg-referral">
-          <div className="org-referral-page">
-            <div className="container">
+          <section className="org-referral-page">
+            <section className="container">
               {!isMobile(screenSize) && <Breadcrumbs links={breadcrumb} />}
-              <h2 className="title-referral">Indique a Printi</h2>
+              <h2 className="title-referral">{'Indique a Printi'}</h2>
 
-              <div className="mol-texts-referral">
-                <h3 className="qrk-title">Quem indica ganha. E quem é indicado também!</h3>
-                <p className="qrk-text">Isso mesmo: indique a Printi para seus amigos, a cada um que comprar, você e ele ganham créditos para trocar por nossos produtos.</p>
-              </div>
+              <section className="mol-texts-referral">
+                <h3 className="qrk-title">{'Quem indica ganha. E quem é indicado também!'}</h3>
+                <p className="qrk-text">
+                  {`Isso mesmo: indique a Printi para seus amigos, e a cada compra feita por eles utilizando
+                  seu código promocional (voucher), você receberá um voucher de desconto!`}
+                </p>
+              </section>
 
-              <div className="mol-content-share">
-                <p className="qrk-text-share">Compartilhe seu código promocional:</p>
-                <form className="mol-share" onSubmit={this.handleSubmit}>
-                  <div className="atm-input-link">
-                    <Input
-                      name={name}
-                      value=""
-                      onChange={this.handleChange}
-                      className="atm-input"
-                    />
-                    <Link to="#" className="atm-link-copy">
-                      copiar
+              <section className="mol-content-share">
+                <p className="qrk-text-share">
+                  {
+                    isLoggedIn ?
+                      'Compartilhe seu código promocional:'
+                      : 'Faça o login ou se cadastre para compartilhar seu código promocional com seus amigos!'
+                  }
+                </p>
+                {
+                  isLoggedIn ?
+                    <form className="mol-share" onSubmit={this.handleSubmit}>
+                      <section className="atm-input-link">
+                        <Input
+                          id="promo_code"
+                          name="promo_code"
+                          defaultValue={voucher_name || voucher_id}
+                          value={voucher_name || voucher_id}
+                          className="atm-input"
+                          readOnly={true}
+                        />
+                        <EnhancedCopyButton
+                          keyProp={'promo-code-copy-tooltip'}
+                          text={'Voucher copiado'}
+                        />
+                      </section>
+                      <button
+                        className="btn-default btn-secondary fnt-sbold btn-sm"
+                      >
+                        {'Compartilhar'}
+                      </button>
+                    </form> :
+                    <Link to={'/login-cadastro'} className="btn-default btn-secondary fnt-sbold btn-sm">
+                      {'Entrar/Cadastrar'}
                     </Link>
-                  </div>
-                  <button className="btn-default btn-secondary fnt-sbold btn-sm">
-                      Compartilhar
-                  </button>
-                </form>
-              </div>
-            </div>
-            <div className="mol-infos-referral">
-              <div className="container">
-                <div className="atm-infos">
-                  <p>Depois de convidar seus amigos, você poderá visualizar o statos das indicações.</p>
-                  <p>Em caso de dúvida, visite a nossa <Link to="/central-de-ajuda">Central de ajuda</Link></p>
-                </div>
-              </div>
-            </div>
-          </div>
-
+                }
+              </section>
+            </section>
+            <section className="mol-infos-referral">
+              <section className="container">
+                <section className="atm-infos">
+                  <p>{'Depois de convidar seus amigos, você poderá visualizar o statos das indicações.'}</p>
+                  <p>{'Em caso de dúvida, visite a nossa'}<Link to="/central-de-ajuda">{'Central de ajuda'}</Link></p>
+                </section>
+              </section>
+            </section>
+          </section>
         </section>
-      </div>
+      </section>
     );
   }
 }
 
-function mapStoreToProps(state) {
+const mapStoreToProps = (state) => {
+  const { LANGUAGE: language, COUNTRY_CODE: countryCode } = state.locale;
+  const { socials } = state.socialLoginSettings;
+  const { voucher_id, voucher_name } = state.account;
   return ({
     app: state.app,
+    customerInfo: {
+      ...state.user.customerInfo,
+      voucher_id,
+      voucher_name,
+    },
+    referral: state.referral,
+    facebook: socials.facebook[countryCode],
+    language,
   });
-}
+};
 
 /* istanbul ignore next */
-function mapDispatchToProps(dispatch) {
-  return { dispatch };
-}
+const mapDispatchToProps = dispatch => ({
+  ...bindActionCreators(ReferralActions, dispatch),
+  socialLoginSettingsFetch: () => dispatch(socialLoginSettingsFetch()),
+});
 
 export default connect(mapStoreToProps, mapDispatchToProps)(Referral);
