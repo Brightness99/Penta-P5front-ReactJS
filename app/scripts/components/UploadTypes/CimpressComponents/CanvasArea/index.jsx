5fadd73be9f95337dba24e31c5d88c9ce99a8c89
Boikov <boikovdmitriy@mail.ru>
2017-11-05 23:58:07 +0400

    Sprint 044 | AECOM-87 | refactoring for canvas

--- app/scripts/components/UploadTypes/CimpressComponents/CanvasArea/index.jsx
+++ app/scripts/components/UploadTypes/CimpressComponents/CanvasArea/index.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const CanvasArea = () => (
   <div className="upload__canvas-schema__canvas-area">
-    <div id="canvases" className="upload__canvas-schema_main-area-container_canvases" />
+    <div id="canvases" className="upload__canvas-schema__canvas-area__canvases" />
   </div>
 );
 
