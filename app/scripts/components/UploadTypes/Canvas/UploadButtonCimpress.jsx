1bc963e14e34a34db81e87f11878d7128307fbe9
Boikov <boikovdmitriy@mail.ru>
2017-11-05 17:33:37 +0400

    Sprint 044 | AECOM-87 | refactoring for canvas

---
+++ app/scripts/components/UploadTypes/Canvas/UploadButtonCimpress.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UploadButtonCimpress = () => (
+  <button className="side-image-bar__button-container_button">
+    <i className="files-icon">+</i>
+    <span>Adicionar novo arquivo</span>
+  </button>
+);
+
+export default UploadButtonCimpress;
+
