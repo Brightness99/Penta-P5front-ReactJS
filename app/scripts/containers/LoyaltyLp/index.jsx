badfbe06d708db13d0b78bd48414a33d5e93f4da
Natália Azevedo <natalia.azevedo@printi.com.br>
2017-11-06 18:23:21 -0200

    Sprint044 | AECOM-131 | start mobile structure and stylesheet

--- app/scripts/containers/LoyaltyLp/index.jsx
+++ app/scripts/containers/LoyaltyLp/index.jsx
@@ -1,38 +1,47 @@
+// @flow
+
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { BannerLoyalty } from './BannerLoyalty';
 import { MemberLoyalty } from './MemberLoyalty';
 import { BenefitsMembers } from './BenefitsMembers';
 import { WhatLost } from './WhatLost';
 import { StartPremium } from './StartPremium';
 
+type Props = {
+  app: AppStore,
+  router: RouterStore,
+  locale: {},
+  dispatch: () => {},
+};
+
 export class LoyaltyLp extends React.Component {
+  static props: Props;
+
   render() {
+    const { app: { screenSize } } = this.props;
     return (
       <section className="tpl-loyalty-lp">
         <div className="org-loyalty-lp">
-
           <div className="mol-banner-member-loyalty">
-            <BannerLoyalty />
-            <MemberLoyalty />
+            <BannerLoyalty screenSize={screenSize} />
+            <MemberLoyalty screenSize={screenSize} />
           </div>
-
-          {/* final container tipos de programa de fidelidade*/}
           <BenefitsMembers />
-          {/* final container tipos de programa de fidelidade*/}
-
-          {/* começo container o que está perdendo */}
           <WhatLost />
-          {/* final container o que está perdendo */}
-
-          {/* começo container ser premium */}
           <StartPremium />
-          {/* final container ser premium */}
-
         </div>
       </section>
     );
   }
 }
 
-export default LoyaltyLp;
+function mapStateToProps(state) {
+  return { app: state.app };
+}
+
+function mapDispatchToProps(dispatch) {
+  return { dispatch };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoyaltyLp);
