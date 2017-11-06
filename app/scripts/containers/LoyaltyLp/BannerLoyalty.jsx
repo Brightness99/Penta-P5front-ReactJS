7bd61211265a196bf5277db57d2e86f6ebc7d58d
Natália Azevedo <natalia.azevedo@printi.com.br>
2017-11-06 18:23:21 -0200

    Sprint044 | AECOM-131 | start mobile structure and stylesheet

--- app/scripts/containers/LoyaltyLp/BannerLoyalty.jsx
+++ app/scripts/containers/LoyaltyLp/BannerLoyalty.jsx
@@ -1,12 +1,23 @@
 // @flow
 import React from 'react';
+import { isMobile } from 'utils/helpers';
 import { PrintiClub } from 'components/Icons';
 
+type Props ={
+  screenSize: string,
+};
 export class BannerLoyalty extends React.Component {
+  props: Props;
+
   render() {
+    const { screenSize } = this.props;
     return (
       <div className="atm-banner-loyalty">
-        <img src={require('assets/media/images/photo-fidelidade.png')} alt="Programa de Fidelidade" />
+        {!isMobile(screenSize) ?
+          <img src={require('assets/media/images/photo-fidelidade.png')} alt="Programa de Fidelidade" />
+          :
+          <img src={require('assets/media/images/photo-fidelidade-mobile.png')} alt="Programa de Fidelidade" />
+        }
         <div className="qrk-banner-text">
           <PrintiClub />
           <h2 className="title-banner-text">Mais vantagens para quem é Printi de carteirinha.</h2>
