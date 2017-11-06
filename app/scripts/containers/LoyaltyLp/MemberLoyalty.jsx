7ce69982f61dc5a1ae9c82482eabb10e5850de13
Natália Azevedo <natalia.azevedo@printi.com.br>
2017-11-06 17:36:46 -0200

    Sprint044 | AECOM-131 | the layout about desktop structure and stylesheet

--- app/scripts/containers/LoyaltyLp/MemberLoyalty.jsx
+++ app/scripts/containers/LoyaltyLp/MemberLoyalty.jsx
@@ -1,6 +1,8 @@
 // @flow
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ChevronDownIcon } from 'components/Icons';
 
 export class MemberLoyalty extends React.Component {
   render() {
@@ -9,22 +11,49 @@ export class MemberLoyalty extends React.Component {
         <div className="atm-member-loyalty">
           <h3 className="title-loyalty">Como se tornar membro</h3>
           <p className="subtitle-loyalty">Muito fácil, como tudo por aqui:</p>
+
           <div className="qrk-be-member">
-            <div className="qrk-">
+            <div className="img-desc-be-member">
               <div>
                 <img src={require('assets/media/images/member-loyalty1.png')} alt="Membro" />
               </div>
-              <div>
-                <p>texto</p>
+              <div className="text-be-member">
+                <p className="first-text-be-member">Pronto, você já é um membro! \o/</p>
+                <p className="second-text-be-member">Aí é só aproveitar todas as promoções exclusivas que reservamos para nossos clientes de carteirinha.</p>
               </div>
             </div>
+
             <div className="line-dashed">
-              line
+              <div className="icon-into-line">
+                <span>Club</span>
+              </div>
+
+              <div className="icon-into-line">
+                <span>Club</span>
+              </div>
+
+              <div className="icon-into-line">
+                <img src={require('assets/media/images/icon-club.png')} alt="Club" />
+              </div>
             </div>
-            <div className="qrk-">
-              <img src={require('assets/media/images/member-loyalty2.png')} alt="Membro" />
+
+            <div className="img-desc-be-member">
+              <div className="text-be-member">
+                <p className="first-text-be-member">R$500 em compras</p>
+                <p className="second-text-be-member">Pode ser em quantas compras quiser, o importante é que seja dentro do intervalo de um ano.</p>
+              </div>
+              <div>
+                <img src={require('assets/media/images/member-loyalty2.png')} alt="Membro" />
+              </div>
             </div>
           </div>
+
+          <div className="qrk-link-scroll-be-member">
+            <h3 className="first-text-scroll-be-member">Como se tornar membro premium?</h3>
+            <p className="second-text-scroll-be-member">Entenda as regras e acomule benefícios</p>
+            <Link to="#"><ChevronDownIcon /></Link>
+          </div>
+
         </div>
       </div>
     );
