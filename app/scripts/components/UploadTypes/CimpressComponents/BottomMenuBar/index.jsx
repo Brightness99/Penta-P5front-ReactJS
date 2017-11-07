8946ea135044a90df65cfdf8f4a3ac4e08b42f96
Boikov <boikovdmitriy@mail.ru>
2017-11-07 14:06:34 +0400

    Sprint 044 | AECOM-87 | added cut view button

--- app/scripts/components/UploadTypes/CimpressComponents/BottomMenuBar/index.jsx
+++ app/scripts/components/UploadTypes/CimpressComponents/BottomMenuBar/index.jsx
@@ -1,12 +1,15 @@
 // @flow
 import React from 'react';
+import { EyeEmptyIcon } from 'components/Icons';
 
 type Props = {
   handleSave: (data) => void,
   handleSaveError: (data) => void,
+  handlePreview: () => void,
+  hasCutPreview: boolean,
 }
 
-const BottomMenuBar = ({ handleSave, handleSaveError }: Props) => {
+const BottomMenuBar = ({ handleSave, handleSaveError, hasCutPreview, handlePreview }: Props) => {
   const saveTemplate = () => {
     global.designer.saveDocumentToUds().then(
       (templateResponse) => handleSave(templateResponse.documentReferenceUrl),
@@ -16,7 +19,14 @@ const BottomMenuBar = ({ handleSave, handleSaveError }: Props) => {
 
   return (
     <div className="upload__canvas-schema__bottom-menu-bar">
-      <section className="dcl-widget-preview-document" />
+      {
+        hasCutPreview ?
+          <button className="canvas-schema__preview-button" onClick={handlePreview}>
+            <EyeEmptyIcon className="" />
+            <span className="description">Visualizar</span>
+          </button>
+          : <section className="dcl-widget-preview-document" />
+      }
       <button className="bottom-menu-bar_btn bottom-menu-bar_btn-finish" onClick={saveTemplate}>
         Finalizar arte
       </button>
