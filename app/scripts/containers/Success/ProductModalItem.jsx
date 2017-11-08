e06679d9e64932d41ba8bc3dd9efb016d08ca1d6
Adam Hola <adam_holman51@gmail.com>
2017-11-08 11:48:14 +0100

    Update success page

--- app/scripts/containers/Success/ProductModalItem.jsx
+++ app/scripts/containers/Success/ProductModalItem.jsx
@@ -19,7 +19,7 @@ export class ProductModalItem extends React.Component {
         <div className="product-item-row">
           <div className="product-item-col-product modal-content">
             {(item.info.thumbnail === '' || !item.info.thumbnail) && <img className="preview" src={require('assets/media/images/blue-logo.png')} alt="Product" />}
-            {item.info.thumbnail !== '' && item.info.thumbnail && <img src={item.info.thumbnail} alt="Product" />}
+            {item.info.thumbnail !== '' && item.info.thumbnail && <img className="preview" src={item.info.thumbnail} alt="Product" />}
             <div>
               <p><b>{item.info.alias}</b></p>
               <p className="content"><span>{item.info.parts[0].part}</span>: {item.info.parts[0].opt_string}</p>
