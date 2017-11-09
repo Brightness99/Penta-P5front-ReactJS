291eb0401072ef0e0cb78b9d5c3c6d3773953505
Natália Azevedo <natalia.azevedo@printi.com.br>
2017-11-09 17:52:53 -0200

    Sprint044 | AECOM-105 | structuring the mobile

---
+++ app/scripts/containers/Tutorial/CardsList.jsx
@@ -0,0 +1,152 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { HourglassIcon, CalendarIcon } from 'components/Icons';
+
+export class CardsList extends React.Component {
+  render() {
+    return (
+      <div className="atm-content-tutorial">
+
+        <div>
+          <div className="qrk-content-tutorial">
+            <NavLink to="#" target="_blank">
+              <div className="details-content-tutorial">
+                <img src={require('assets/media/images/tutorial-list-li.png')} alt="Turorial" />
+                <div className="desc-content-tutorial">
+                  <span className="cat-content-tutorial bg-tools">Ferramentas</span>
+                  <h3 className="title-desc-content-tutorial">Como fazer download dos gabaritos disponíveis</h3>
+                  <div className="footer-content-tutorial">
+                    <div className="date-time-content-tutorial">
+                      <CalendarIcon />
+                      <span>7 Out</span>
+                    </div>
+                    <div className="date-time-content-tutorial">
+                      <HourglassIcon />
+                      <span>6min para ler</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </NavLink>
+          </div>
+
+          <div className="qrk-content-tutorial">
+            <NavLink to="#" target="_blank">
+              <div className="details-content-tutorial">
+                <img src={require('assets/media/images/tutorial-list-li.png')} alt="Turorial" />
+                <div className="desc-content-tutorial">
+                  <span className="cat-content-tutorial bg-product">Produto</span>
+                  <h3 className="title-desc-content-tutorial">Como fazer download dos gabaritos disponíveis</h3>
+                  <div className="footer-content-tutorial">
+                    <div className="date-time-content-tutorial">
+                      <CalendarIcon />
+                      <span>7 Out</span>
+                    </div>
+                    <div className="date-time-content-tutorial">
+                      <HourglassIcon />
+                      <span>6min para ler</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </NavLink>
+          </div>
+
+          <div className="qrk-content-tutorial">
+            <NavLink to="#" target="_blank">
+              <div className="details-content-tutorial">
+                <img src={require('assets/media/images/tutorial-list-li.png')} alt="Turorial" />
+                <div className="desc-content-tutorial">
+                  <span className="cat-content-tutorial bg-videos">Vídeos</span>
+                  <h3 className="title-desc-content-tutorial">Como fazer download dos gabaritos disponíveis</h3>
+                  <div className="footer-content-tutorial">
+                    <div className="date-time-content-tutorial">
+                      <CalendarIcon />
+                      <span>7 Out</span>
+                    </div>
+                    <div className="date-time-content-tutorial">
+                      <HourglassIcon />
+                      <span>6min para ler</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </NavLink>
+          </div>
+
+          <div className="qrk-content-tutorial">
+            <NavLink to="#" target="_blank">
+              <div className="details-content-tutorial">
+                <img src={require('assets/media/images/tutorial-list-li.png')} alt="Turorial" />
+                <div className="desc-content-tutorial">
+                  <span className="cat-content-tutorial bg-tools">Ferramentas</span>
+                  <h3 className="title-desc-content-tutorial">Como fazer download dos gabaritos disponíveis</h3>
+                  <div className="footer-content-tutorial">
+                    <div className="date-time-content-tutorial">
+                      <CalendarIcon />
+                      <span>7 Out</span>
+                    </div>
+                    <div className="date-time-content-tutorial">
+                      <HourglassIcon />
+                      <span>6min para ler</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </NavLink>
+          </div>
+
+          <div className="qrk-content-tutorial">
+            <NavLink to="#" target="_blank">
+              <div className="details-content-tutorial">
+                <img src={require('assets/media/images/tutorial-list-li.png')} alt="Turorial" />
+                <div className="desc-content-tutorial">
+                  <span className="cat-content-tutorial bg-product">Produto</span>
+                  <h3 className="title-desc-content-tutorial">Como fazer download dos gabaritos disponíveis</h3>
+                  <div className="footer-content-tutorial">
+                    <div className="date-time-content-tutorial">
+                      <CalendarIcon />
+                      <span>7 Out</span>
+                    </div>
+                    <div className="date-time-content-tutorial">
+                      <HourglassIcon />
+                      <span>6min para ler</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </NavLink>
+          </div>
+
+          <div className="qrk-content-tutorial">
+            <NavLink to="#" target="_blank">
+              <div className="details-content-tutorial">
+                <img src={require('assets/media/images/tutorial-list-li.png')} alt="Turorial" />
+                <div className="desc-content-tutorial">
+                  <span className="cat-content-tutorial bg-videos">Vídeos</span>
+                  <h3 className="title-desc-content-tutorial">Como fazer download dos gabaritos disponíveis</h3>
+                  <div className="footer-content-tutorial">
+                    <div className="date-time-content-tutorial">
+                      <CalendarIcon />
+                      <span>7 Out</span>
+                    </div>
+                    <div className="date-time-content-tutorial">
+                      <HourglassIcon />
+                      <span>6min para ler</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </NavLink>
+          </div>
+        </div>
+
+        <div className="see-more-tutorial">
+          <button className="btn-default btn-third btn-xs">Carregar mais artigos (3)</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CardsList;
