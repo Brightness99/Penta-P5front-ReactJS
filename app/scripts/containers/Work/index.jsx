fdee7a335128d319d5fcb4fcd44495e931a26ac2
Natália Azevedo <natalia.azevedo@printi.com.br>
2017-11-14 13:15:37 -0200

    Sprint044 | AECOM-106 | Layout mobile (doing)

--- app/scripts/containers/Work/index.jsx
+++ app/scripts/containers/Work/index.jsx
@@ -1,23 +1,42 @@
+// @flow
 import React from 'react';
-
+import { connect } from 'react-redux';
 import BannerWork from './BannerWork';
 import DetailsWork from './DetailsWork';
 import OwnersComment from './OwnersComment';
 import WeSearch from './WeSearch';
 import LookJobs from './LookJobs';
 
+type Props = {
+  app: AppStore,
+  router: RouterStore,
+  locale: {},
+  dispatch: () => {},
+};
+
 export class Work extends React.Component {
+  static props: Props;
+
   render() {
+    const { app: { screenSize } } = this.props;
     return (
       <section className="tpl-work-us">
-        <BannerWork />
-        <DetailsWork />
+        <BannerWork screenSize={screenSize} />
+        <DetailsWork screenSize={screenSize} />
         <OwnersComment />
         <WeSearch />
-        <LookJobs />
+        <LookJobs screenSize={screenSize} />
       </section>
     );
   }
 }
 
-export default Work;
+function mapStateToProps(state) {
+  return { app: state.app };
+}
+
+function mapDispatchToProps(dispatch) {
+  return { dispatch };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Work);
